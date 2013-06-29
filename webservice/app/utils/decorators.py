b6842940d5308851c5c30e15cca91454bd4037fa
mjjc <mjjc1337@gmail.com>
2013-06-29 19:48:01 +0200

    json support

--- webservice/app/utils/decorators.py
+++ webservice/app/utils/decorators.py
@@ -1,17 +1,22 @@
 from uuid import validate
-from flask import request, abort
+from flask import request, abort,jsonify
 from functools import wraps
 
-def require_uuid(key):
+def require_uuid(arg=None, json=None):
     '''
     Checks whether a request contains specified argument of UUID type.
     '''
     def decorator(func):
         @wraps(func)
         def new_func(*args, **kwargs):
-            kwargs[key] = request.values.get(key)
-            if not validate(request.values.get(key)):
-                abort(400)
+            if arg:
+                kwargs[arg] = request.values.get(arg)
+                if not validate(kwargs[arg]):
+                    abort(400)
+            if json:
+                kwargs[json] = request.json.get(json)
+                if not validate(kwargs[json]):
+                    abort(400)
             return func(*args, **kwargs)
         return new_func
     return decorator
--- webservice/app/views.py
+++ webservice/app/views.py
@@ -14,7 +14,7 @@ def show_review(review_id):
     response = jsonify(review=review.to_dict())
     return response
     
-@require_uuid('release_group')
+@require_uuid(arg='release_group')
 def list_review(release_group):
     ''' Fetching a list of reviews relevant to a specified release group '''
     reviews = db.session.query(Review).filter(Review.release_group == release_group).\
@@ -22,14 +22,14 @@ def list_review(release_group):
     response = jsonify(reviews=[r.to_dict() for r in reviews])
     return response
 
-@require_uuid('release_group')
-@require_uuid('user_id') # used temporary for tests until oauth is done
+@require_uuid(json='release_group')
+@require_uuid(json='user_id') # used temporary for tests until oauth is done
 def post_review(release_group, user_id):
     ''' Posting a review '''
     user = User.query.get(user_id)
-    text = request.values.get('text') 
+    text = request.json.get('text') 
     review = Review(user, text, release_group)
     db.session.add(review)
     db.session.commit()
-    response = jsonify(review=dict(id=review.id))
+    response = jsonify(review=review.to_dict())
     return response
