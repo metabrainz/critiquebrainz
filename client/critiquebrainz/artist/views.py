e04b652cbc4c56274152c3984d0165a5d0b3a058
Roman Tsukanov <roman@tsukanov.me>
2014-05-02 20:24:21 +1100

    Changed default limit on artist page.

--- client/critiquebrainz/artist/views.py
+++ client/critiquebrainz/artist/views.py
@@ -8,7 +8,7 @@ bp = Blueprint('artist', __name__)
 @bp.route('/<uuid:id>', endpoint='entity')
 def artist_entity_handler(id):
     artist = musicbrainz.artist_details(id)
-    limit = int(request.args.get('limit', default=20))
+    limit = int(request.args.get('limit', default=30))
     offset = int(request.args.get('offset', default=0))
     count, releases = musicbrainz.browse_release_groups(artist=id, limit=limit, offset=offset)
     return render_template('artist.html', id=id, artist=artist, releases=releases,
