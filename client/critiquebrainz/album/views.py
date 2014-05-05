8f8bf8e0aa9a116f5a7fac80bfcec53f7fa8fd2f
Roman Tsukanov <roman@tsukanov.me>
2014-05-06 05:52:41 +1100

    Increased default review limit on artist page.

--- client/critiquebrainz/album/views.py
+++ client/critiquebrainz/album/views.py
@@ -9,7 +9,7 @@ bp = Blueprint('album', __name__)
 @bp.route('/<uuid:id>', endpoint='entity')
 def release_group_entity_handler(id):
     album = musicbrainz.album_details(id)
-    limit = int(request.args.get('limit', default=5))
+    limit = int(request.args.get('limit', default=10))
     offset = int(request.args.get('offset', default=0))
     count, reviews = api.get_reviews(release_group=id, sort='created',
                                      limit=limit, offset=offset, inc=['user'])
