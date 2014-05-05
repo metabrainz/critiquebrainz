bc170b8ca9b5f16e259d272aa231c81955ef44a6
Roman Tsukanov <roman@tsukanov.me>
2014-05-06 06:51:26 +1100

    Checking if user already wrote review for the album.

--- client/critiquebrainz/album/views.py
+++ client/critiquebrainz/album/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request
+from flask.ext.login import current_user
+
 from critiquebrainz.api import api
 from critiquebrainz import musicbrainz
 
@@ -11,7 +13,12 @@ def release_group_entity_handler(id):
     album = musicbrainz.album_details(id)
     limit = int(request.args.get('limit', default=10))
     offset = int(request.args.get('offset', default=0))
+    my_count, my_reviews = api.get_reviews(release_group=id, user_id=current_user.me['id'])
+    if my_count != 0:
+        my_review = my_reviews[0]
+    else:
+        my_review = None
     count, reviews = api.get_reviews(release_group=id, sort='created',
                                      limit=limit, offset=offset, inc=['user'])
-    return render_template('album.html', id=id, album=album, reviews=reviews,
+    return render_template('album.html', id=id, album=album, reviews=reviews, my_review=my_review,
                            limit=limit, offset=offset, count=count)
