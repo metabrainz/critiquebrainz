b619349599c18ac137319508a76726b48ca7a1c1
Roman Tsukanov <roman@tsukanov.me>
2014-05-08 04:49:51 +1100

    Updated home page layout. Added some text. Added list of most popular reviews. Added "Write a review" button.

--- client/critiquebrainz/views.py
+++ client/critiquebrainz/views.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from critiquebrainz import app
 from critiquebrainz.api import api
-from critiquebrainz.exceptions import APIError
+
 
 @app.route('/', endpoint='index')
 def index_handler():
-    _, reviews = api.get_reviews(sort='created', limit=5, inc=['user'])
-    return render_template('index.html', reviews=reviews)
+    _, hot_reviews = api.get_reviews(sort='rating', limit=5, inc=['user'])
+    _, recent_reviews = api.get_reviews(sort='created', limit=5, inc=['user'])
+    return render_template('index.html', hot_reviews=hot_reviews, recent_reviews=recent_reviews)
