d82edf8ef1a19850c832ecc6469769e6dabab729
Roman Tsukanov <roman@tsukanov.me>
2014-05-13 10:29:58 +1100

    Marked review_id column in revision table as indexed.

--- server/critiquebrainz/db/revision.py
+++ server/critiquebrainz/db/revision.py
@@ -7,7 +7,7 @@ class Revision(db.Model):
     __tablename__ = 'revision'
 
     id = db.Column(db.Integer, primary_key=True)
-    review_id = db.Column(UUID, db.ForeignKey('review.id', ondelete='CASCADE'), primary_key=True)
+    review_id = db.Column(UUID, db.ForeignKey('review.id', ondelete='CASCADE'), primary_key=True, index=True)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     text = db.Column(db.Unicode, nullable=False)
 
