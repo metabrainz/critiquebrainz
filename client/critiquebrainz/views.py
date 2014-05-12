fb2f00538fbfb955b1d25d3db5c8613a5da8113e
Roman Tsukanov <roman@tsukanov.me>
2014-05-12 15:21:48 +1100

    Added stats panel on home page.

--- client/critiquebrainz/views.py
+++ client/critiquebrainz/views.py
@@ -6,5 +6,6 @@ from critiquebrainz.api import api
 @app.route('/', endpoint='index')
 def index_handler():
     _, hot_reviews = api.get_reviews(sort='rating', limit=5, inc=['user'])
-    _, recent_reviews = api.get_reviews(sort='created', limit=5, inc=['user'])
-    return render_template('index.html', hot_reviews=hot_reviews, recent_reviews=recent_reviews)
+    reviews_total, recent_reviews = api.get_reviews(sort='created', limit=5, inc=['user'])
+    return render_template('index.html', hot_reviews=hot_reviews, recent_reviews=recent_reviews,
+                           reviews_total=reviews_total)
