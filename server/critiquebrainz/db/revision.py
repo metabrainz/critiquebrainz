42585b1a546a57baaecf5a255b3ce2c27ca2effb
Roman Tsukanov <roman@tsukanov.me>
2014-05-14 06:17:08 +1100

    Integrated revisions into voting system.

--- server/critiquebrainz/db/revision.py
+++ server/critiquebrainz/db/revision.py
@@ -7,9 +7,11 @@ class Revision(db.Model):
     __tablename__ = 'revision'
 
     id = db.Column(db.Integer, primary_key=True)
-    review_id = db.Column(UUID, db.ForeignKey('review.id', ondelete='CASCADE'), primary_key=True, index=True)
+    review_id = db.Column(UUID, db.ForeignKey('review.id', ondelete='CASCADE'), index=True)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     text = db.Column(db.Unicode, nullable=False)
+
+    _votes = db.relationship('Vote', cascade='delete', lazy='dynamic', backref='revision')
 
     def to_dict(self):
         response = dict(id=self.id,
--- server/critiquebrainz/db/vote.py
+++ server/critiquebrainz/db/vote.py
@@ -1,12 +1,13 @@
-from . import db
-from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
+from . import db
+
 
 class Vote(db.Model):
-
     __tablename__ = 'vote'
     user_id = db.Column(UUID, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
     review_id = db.Column(UUID, db.ForeignKey('review.id', ondelete='CASCADE'), primary_key=True)
+    revision_id = db.Column(db.Integer, db.ForeignKey('revision.id', ondelete='CASCADE'), primary_key=True)
     placet = db.Column(db.Boolean, nullable=False)
     rated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
@@ -15,10 +16,11 @@ class Vote(db.Model):
         # changing votes of an archived review is forbidden
         if review.is_archived is True:
             return
-        # delete the vote if it exists
+            # delete the vote if it exists
         cls.query.filter_by(user=user, review=review).delete()
         # create a new vote
-        vote = cls(user=user, review=review, placet=placet)
+        last_revision = review.revisions[-1]
+        vote = cls(user=user, review=review, revision=last_revision, placet=placet)
         db.session.add(vote)
         db.session.commit()
 
@@ -33,6 +35,5 @@ class Vote(db.Model):
         return vote
 
     def to_dict(self):
-        response = dict(placet=self.placet,
-            voted_at=self.rated_at)
+        response = dict(placet=self.placet, voted_at=self.rated_at)
         return response
