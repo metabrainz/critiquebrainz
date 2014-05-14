4ca0a3c009ed29b43b0f3430cdae682ec12e4a25
Roman Tsukanov <roman@tsukanov.me>
2014-05-15 10:28:15 +1100

    Added support for icons in Release Group-URL relationship. Adjusted process of new dictionary creation.

--- client/critiquebrainz/relationships.py
+++ client/critiquebrainz/relationships.py
@@ -1,48 +1,41 @@
 def process_relationship(type_a, type_b, list=[]):
     supported_types = supported_relations[type_a][type_b]
-    new_rels = []
+    processed = []
     for relation in list:
         if relation['type'] in supported_types:
             rel_type = supported_types[relation['type']]
-            new_rels.append({
-                'type': relation['type'],
-                'type-id': relation['type-id'],
-                'target': relation['target'],
-                'name': rel_type['name'],
-            })
-    return new_rels
+            processed.append(dict(relation.items() + rel_type.items()))
+    return processed
 
 
+# All advanced relationships supported by CritiqueBrainz are defined here.
 supported_relations = {
     'release-group': {
         'url': {
             'wikipedia': {
-                'id': '6578f0e9-1ace-4095-9de8-6e517ddb1ceb',
-                'name': 'Wikipedia'
+                'name': 'Wikipedia',
+                'icon': 'wikipedia-16.png',
             },
             'wikidata': {
-                'id': 'b988d08c-5d86-4a57-9557-c83b399e3580',
-                'name': 'Wikidata'
+                'name': 'Wikidata',
+                'icon': 'wikidata-16.png',
             },
             'discogs': {
-                'id': '99e550f3-5ab4-3110-b5b9-fe01d970b126',
-                'name': 'Discogs'
+                'name': 'Discogs',
+                'icon': 'discogs-16.png',
             },
             'allmusic': {
-                'id': 'a50a1d20-2b20-4d2c-9a29-eb771dd78386',
-                'name': 'Allmusic'
+                'name': 'Allmusic',
+                'icon': 'allmusic-16.png',
             },
             'lyrics': {
-                'id': '156344d3-da8b-40c6-8b10-7b1c22727124',
-                'name': 'Lyrics'
+                'name': 'Lyrics',
             },
             'official homepage': {
-                'id': '87d97dfc-3206-42fd-89d5-99593d5f1297',
-                'name': 'Official homepage'
+                'name': 'Official homepage',
             },
             'recording studio': {
-                'id': 'b17e54df-dcff-4ce3-9ab6-83f4bc0ec50b',
-                'name': 'Recording studio'
+                'name': 'Recording studio',
             },
         },
     },
