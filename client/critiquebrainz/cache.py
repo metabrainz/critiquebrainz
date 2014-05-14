1e42f4b6398e6fc0eb16ac645cce9e6e873281dc
Roman Tsukanov <roman@tsukanov.me>
2014-05-14 18:35:54 +1100

    Moved colon from namespace into cache key generator.

--- client/critiquebrainz/cache.py
+++ client/critiquebrainz/cache.py
@@ -5,7 +5,7 @@ cache = memcache.Client(app.config['MEMCACHED_SERVERS'], debug=0)
 
 
 def generate_cache_key(id, type=None, source=None):
-    key = app.config['MEMCACHED_NAMESPACE']
+    key = app.config['MEMCACHED_NAMESPACE'] + ':'
     if source is not None:
         key += str(source) + ':'
     if type is not None:
