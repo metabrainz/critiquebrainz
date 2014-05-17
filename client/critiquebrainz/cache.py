f6792a634a61b8eefc3d883b62f9cb333d9ff2bb
Roman Tsukanov <roman@tsukanov.me>
2014-05-18 10:06:28 +1100

    Fixed params parser in cache key generator.

--- client/critiquebrainz/cache.py
+++ client/critiquebrainz/cache.py
@@ -11,7 +11,11 @@ def generate_cache_key(id, type=None, source=None, params=[]):
         key += str(source) + ':'
     if type is not None:
         key += str(type) + ':'
-    key += str(id) + '_'.join(map(str, params))
+    key += str(id)
+    for param in params:
+        if not isinstance(param, basestring):
+            param = str(param)
+        key += '_' + param.encode('ascii', 'ignore')
     key = key.replace(' ', '_')
     if len(key) > 250:  # 250 bytes is the maximum key length in memcached
         key = hashlib.sha1(key).hexdigest()
