b6c4480d78ef71c1c219ad16d85e4ef357aa3f85
Roman Tsukanov <roman@tsukanov.me>
2014-06-01 04:31:10 +1100

    Grammar.

--- client/critiquebrainz/forms/profile/review.py
+++ client/critiquebrainz/forms/profile/review.py
@@ -12,7 +12,7 @@ class CreateForm(Form):
             ('CC BY-SA 3.0', gettext('Allow commercial use of this review (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0 license</a>)')),
             ('CC BY-NC-SA 3.0', gettext('Do not allow commercial use of this review, unless approved by MetaBrainz Foundation (<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA 3.0 license</a>)')),
         ],
-        validators=[validators.DataRequired(message=gettext("You need to choose license"))])
+        validators=[validators.DataRequired(message=gettext("You need to choose a license"))])
     licence = BooleanField(gettext('Licence'), validators=[
         validators.DataRequired(message=gettext("You need to accept the licence agreement"))])
 
