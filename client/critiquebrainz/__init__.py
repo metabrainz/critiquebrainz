efbbf274851dded7ea5ffcbb8ae32d9f29690fc0
Roman Tsukanov <roman@tsukanov.me>
2014-06-27 19:16:38 +1000

    Added calls to inject_into_urllib3(). Updated requirements.

--- client/critiquebrainz/__init__.py
+++ client/critiquebrainz/__init__.py
@@ -6,6 +6,14 @@ app_version = "0.1"
 app = Flask(__name__)
 app.config.from_object('critiquebrainz.config')
 
+# SNI support for Python 2
+# See http://urllib3.readthedocs.org/en/latest/contrib.html#module-urllib3.contrib.pyopenssl
+try:
+    import urllib3.contrib.pyopenssl
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
+except ImportError:
+    pass
+
 # init apis
 import apis
 
--- server/critiquebrainz/__init__.py
+++ server/critiquebrainz/__init__.py
@@ -12,6 +12,14 @@ else:
 _name = "CritiqueBrainz Server"
 __version__ = "0.1"
 
+# SNI support for Python 2
+# See http://urllib3.readthedocs.org/en/latest/contrib.html#module-urllib3.contrib.pyopenssl
+try:
+    import urllib3.contrib.pyopenssl
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
+except ImportError:
+    pass
+
 
 def create_app(config_object=None):
     app = Flask(__name__)
