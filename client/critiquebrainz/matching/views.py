d2af69aef86ab417723b2e9fcebdee7dddfba7d5
Roman Tsukanov <roman@tsukanov.me>
2014-06-30 00:30:10 +1000

    Removing punctuation from Spotify search queries.

--- client/critiquebrainz/matching/views.py
+++ client/critiquebrainz/matching/views.py
@@ -1,3 +1,4 @@
+import string
 from flask import Blueprint, render_template, request, url_for, redirect, flash
 from flask.ext.login import login_required, current_user
 from flask.ext.babel import gettext
@@ -19,7 +20,8 @@ def spotify_matching_handler(release_group_id):
     release_group = musicbrainz.release_group_details(release_group_id)
     limit = 20
     offset = int(request.args.get('offset', default=0))
-    response = spotify.search(release_group['title'], 'album', limit, offset).get('albums')
+    query = release_group['title'].translate(string.maketrans("", ""), string.punctuation)
+    response = spotify.search(query, 'album', limit, offset).get('albums')
     count, search_results = response.get('total'), response.get('items')
     return render_template('matching/spotify.html', release_group=release_group, search_results=search_results,
                            limit=limit, offset=offset, count=count)
