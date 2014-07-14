c432b76404c848951c5502db86c572f7ca220754
Roman Tsukanov <roman@tsukanov.me>
2014-07-15 02:32:57 +1000

    Removed duplicate code by subclassing TestCase.

---
+++ server/critiquebrainz/test_case.py
@@ -0,0 +1,19 @@
+from flask.ext.testing import TestCase
+from critiquebrainz import create_app
+from critiquebrainz.db import db
+
+
+class ServerTestCase(TestCase):
+
+    def create_app(self):
+        app = create_app()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = app.config['TEST_SQLALCHEMY_DATABASE_URI']
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
--- server/run_tests.py
+++ server/run_tests.py
@@ -1,10 +1,10 @@
 import unittest
 import manage
-import test_config
+from critiquebrainz import app
 
 if __name__ == '__main__':
     # Creating database-related stuff
-    manage.init_postgres(test_config.SQLALCHEMY_DATABASE_URI)
+    manage.init_postgres(app.config['TEST_SQLALCHEMY_DATABASE_URI'])
 
     all = unittest.TestLoader().discover(start_dir='critiquebrainz', pattern='*_test.py')
     unittest.TextTestRunner(verbosity=1).run(all)
