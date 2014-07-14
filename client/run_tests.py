fc7251268fdade68a540c50e2d32cfc3f6bc0101
Roman Tsukanov <roman@tsukanov.me>
2014-07-15 02:59:11 +1000

    Fixed start_dir in run_tests modules.

--- client/run_tests.py
+++ client/run_tests.py
@@ -1,5 +1,5 @@
 import unittest
 
 if __name__ == '__main__':
-    all = unittest.TestLoader().discover(start_dir='critiquebrainz', pattern='*_test.py')
+    all = unittest.TestLoader().discover(start_dir='.', pattern='*_test.py')
     unittest.TextTestRunner(verbosity=1).run(all)
--- server/run_tests.py
+++ server/run_tests.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     # Creating database-related stuff
     manage.init_postgres(app.config['TEST_SQLALCHEMY_DATABASE_URI'])
 
-    all = unittest.TestLoader().discover(start_dir='critiquebrainz', pattern='*_test.py')
+    all = unittest.TestLoader().discover(start_dir='.', pattern='*_test.py')
     unittest.TextTestRunner(verbosity=1).run(all)
