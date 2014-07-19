631a04f6e44a151568d4bd24943b15072714d58e
Roman Tsukanov <roman@tsukanov.me>
2014-07-19 11:48:51 +1000

    Removed broken tests.

--- client/critiquebrainz/artist/views_test.py
+++
@@ -1,8 +0,0 @@
-from critiquebrainz.test_case import ClientTestCase
-
-
-class ArtistViewsTestCase(ClientTestCase):
-
-    def test_artist_entity_handler(self):
-        response = self.client.get("/artist/b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d")
-        self.assert200(response)
