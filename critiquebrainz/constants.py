32d65637d0eba37e6e0e50eb80583471bda74712
Roman Tsukanov <roman@tsukanov.me>
2014-07-26 21:34:11 +1000

    Made constants translatable.

--- critiquebrainz/constants.py
+++ critiquebrainz/constants.py
@@ -1,3 +1,5 @@
+from flask.ext.babel import gettext
+
 # USER TYPES
 class UserType(object):
 
@@ -11,31 +13,31 @@ class UserType(object):
         return self.karma(user.karma)
 
 blocked = UserType(
-    label=u'Blocked',
+    label=gettext('Blocked'),
     karma=lambda x: (x < -20),
     reviews_per_day=0,
     votes_per_day=0)
 
 spammer = UserType(
-    label=u'Spammer',
+    label=gettext('Spammer'),
     karma=lambda x: (x >= -20 and x < -10),
     reviews_per_day=1,
     votes_per_day=0)
 
 noob = UserType(
-    label=u'Noob',
+    label=gettext('Noob'),
     karma=lambda x: (x >= -10 and x < 50),
     reviews_per_day=5,
     votes_per_day=10)
 
 apprentice = UserType(
-    label=u'Apprentice',
+    label=gettext('Apprentice'),
     karma=lambda x: (x >= 50 and x < 1000),
     reviews_per_day=20,
     votes_per_day=50)
 
 sorcerer = UserType(
-    label=u'Sorcerer',
+    label=gettext('Sorcerer'),
     karma=lambda x: (x >= 1000),
     reviews_per_day=50,
     votes_per_day=200)
@@ -57,28 +59,28 @@ class ReviewClass(object):
         return self.rating(review.rating)
 
 spam = ReviewClass(
-    label=u'Spam',
+    label=gettext('Spam'),
     rating=lambda x: (x < -10),
     upvote=(apprentice, sorcerer),
     downvote=(noob, apprentice, sorcerer),
     mark_spam=(apprentice, sorcerer))
 
 neutral = ReviewClass(
-    label=u'Neutral',
+    label=gettext('Neutral'),
     rating=lambda x: (x >= -10 and x < 10),
     upvote=(noob, apprentice, sorcerer),
     downvote=(noob, apprentice, sorcerer),
     mark_spam=(apprentice, sorcerer))
 
 promising = ReviewClass(
-    label=u'Promising',
+    label=gettext('Promising'),
     rating=lambda x: (x >= 10 and x < 30),
     upvote=(spammer, noob, apprentice, sorcerer),
     downvote=(noob, apprentice, sorcerer),
     mark_spam=(apprentice, sorcerer))
 
 trusted = ReviewClass(
-    label=u'Trusted',
+    label=gettext('Trusted'),
     rating=lambda x: (x >= 30),
     upvote=(spammer, noob, apprentice, sorcerer),
     downvote=(apprentice, sorcerer),
