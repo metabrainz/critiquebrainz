692b3d905efa4cd778e9e00f90293fde298812cb
Roman Tsukanov <roman@tsukanov.me>
2014-08-08 10:40:46 +1000

    Added ability to report incorrect Spotify mapping.

--- critiquebrainz/frontend/apis/mbspotify.py
+++ critiquebrainz/frontend/apis/mbspotify.py
@@ -1,30 +1,35 @@
 import json
-
 import requests
-
 from critiquebrainz.frontend.apis.exceptions import APIError
 
 
 class MBSpotifyClient(object):
-    """Provides interface to Spotify ID mapper."""
+    """Provides interface to Spotify ID mapper - MBSpotify.
+    Source code of this application is available at https://github.com/metabrainz/mbspotify.
+    """
 
     def __init__(self, base_url):
         self.base_url = base_url
 
     def mapping(self, mbids=None):
+        """Get mapping to Spotify for a set of MusicBrainz IDs."""
         if mbids is None:
             mbids = []
         try:
             headers = {'Content-type': 'application/json'}
-            resp = requests.post(self.base_url + "mapping", headers=headers,
-                                 data=json.dumps({'mbids': mbids}))
+            resp = requests.post(self.base_url + 'mapping', headers=headers, data=json.dumps({'mbids': mbids}))
             return resp.json().get('mapping')
         except Exception as e:
+            # TODO: Catch errors properly and return informative errors.
             return []
 
     def add_mapping(self, mbid, spotify_uri, user_id):
-        if not self.base_url:
-            raise APIError(desc="MBSpotify server is unavailable.")
-        headers = {'Content-type': 'application/json'}
-        resp = requests.post(self.base_url + "mapping/add", headers=headers,
-                             data=json.dumps({'mbid': str(mbid), 'spotify_uri': spotify_uri, 'user': str(user_id)}))
+        """Submit new spotify mapping."""
+        # TODO: Catch errors during submission.
+        requests.post(self.base_url + 'mapping/add', headers={'Content-Type': 'application/json'},
+                      data=json.dumps({'mbid': str(mbid), 'spotify_uri': spotify_uri, 'user': str(user_id)}))
+
+    def vote(self, mbid, user_id):
+        """Submit report about incorrect Spotify mapping."""
+        requests.post(self.base_url + 'mapping/vote', headers={'Content-Type': 'application/json'},
+                      data=json.dumps({'mbid': str(mbid), 'user': str(user_id)}))
--- critiquebrainz/frontend/matching/views.py
+++ critiquebrainz/frontend/matching/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect, flash
+from flask import Blueprint, render_template, request, url_for, redirect, flash, jsonify
 from flask.ext.login import login_required, current_user
 from flask.ext.babel import gettext
 from critiquebrainz.frontend.apis import musicbrainz, spotify, mbspotify
@@ -69,6 +69,12 @@ def spotify_matching_submit_handler(release_group_id):
 @matching_bp.route('/spotify/<uuid:release_group_id>/confirm', methods=['POST'], endpoint='spotify_submit')
 @login_required
 def spotify_matching_submit_handler(release_group_id):
+    # Checking if release group exists
+    release_group = musicbrainz.release_group_details(release_group_id)
+    if not release_group:
+        flash(gettext("Only existing release groups can be matched to Spotify!"), 'error')
+        return redirect(url_for('search.index'))
+
     # Checking if release group is already matched
     spotify_mapping = mbspotify.mapping([str(release_group_id)])
     if len(spotify_mapping) > 0:
@@ -78,6 +84,23 @@ def spotify_matching_submit_handler(release_group_id):
     spotify_uri = request.args.get('spotify_uri', default=None)
     if not spotify_uri:
         return redirect(url_for('.spotify', release_group_id=release_group_id))
-    message = mbspotify.add_mapping(release_group_id, spotify_uri, current_user.id)
+    mbspotify.add_mapping(release_group_id, spotify_uri, current_user.id)
     flash(gettext('Connection is added!'), 'success')
     return redirect(url_for('release_group.entity', id=release_group_id))
+
+
+@matching_bp.route('/spotify/<uuid:release_group_id>/report', methods=['POST'], endpoint='spotify_report')
+@login_required
+def spotify_matching_report_handler(release_group_id):
+    # Checking if release group exists
+    release_group = musicbrainz.release_group_details(release_group_id)
+    if not release_group:
+        return jsonify(success=False, error=gettext("Can't find release group with that ID!"))
+
+    # Checking if release group is matched
+    spotify_mapping = mbspotify.mapping([str(release_group_id)])
+    if len(spotify_mapping) < 1:
+        return jsonify(success=False, error=gettext("This album is not matched to Spotify yet!"))
+
+    mbspotify.vote(release_group_id, current_user.id)
+    return jsonify(success=True)
