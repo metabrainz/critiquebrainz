7495d4fd415fb3548ebd8bb96df21769d7f776b0
Roman Tsukanov <roman@tsukanov.me>
2014-08-16 13:28:06 +1000

    Added support for more types of references to Spotify that can be used for matching.

--- critiquebrainz/frontend/matching/views.py
+++ critiquebrainz/frontend/matching/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, url_for, redirect, flash,
 from flask.ext.login import login_required, current_user
 from flask.ext.babel import gettext
 from critiquebrainz.frontend.apis import musicbrainz, spotify, mbspotify
+from urlparse import urlparse
+import os.path
 import string
 
 matching_bp = Blueprint('matching', __name__)
@@ -35,7 +37,7 @@ def spotify_matching_handler(release_group_id):
                            page=page, limit=limit, count=response.get('total'))
 
 
-@matching_bp.route('/spotify/<uuid:release_group_id>/confirm', methods=['GET'], endpoint='spotify_confirm')
+@matching_bp.route('/spotify/<uuid:release_group_id>/confirm', methods=['GET', 'POST'], endpoint='spotify_confirm')
 @login_required
 def spotify_matching_submit_handler(release_group_id):
     # Checking if release group is already matched
@@ -49,44 +51,27 @@ def spotify_matching_submit_handler(release_group_id):
         flash(gettext("Only existing release groups can be matched to Spotify!"), 'error')
         return redirect(url_for('search.index'))
 
-    spotify_uri = request.args.get('spotify_uri', default=None)
-    if not spotify_uri:
+    spotify_ref = request.args.get('spotify_ref', default=None)
+    if not spotify_ref:
         flash(gettext("You need to select an album from Spotify!"), 'error')
         return redirect(url_for('.spotify', release_group_id=release_group_id))
 
-    if not spotify_uri.startswith("spotify:album:"):
-        flash(gettext("You need to specify correct Spotify URI for this album!"), 'error')
+    spotify_id = parse_spotify_id(spotify_ref)
+    if not spotify_id:
+        flash(gettext("You need to specify correct reference to Spotify for this album!"), 'error')
         return redirect(url_for('.spotify', release_group_id=release_group_id))
 
-    album = spotify.album(spotify_uri[14:])
+    album = spotify.album(spotify_id)
     if not album or album.get('error'):
         flash(gettext("You need to specify existing album from Spotify!"), 'error')
         return redirect(url_for('.spotify', release_group_id=release_group_id))
 
-    return render_template('matching/confirm.html', release_group=release_group, spotify_album=album)
-
-
-@matching_bp.route('/spotify/<uuid:release_group_id>/confirm', methods=['POST'], endpoint='spotify_submit')
-@login_required
-def spotify_matching_submit_handler(release_group_id):
-    # Checking if release group exists
-    release_group = musicbrainz.release_group_details(release_group_id)
-    if not release_group:
-        flash(gettext("Only existing release groups can be matched to Spotify!"), 'error')
-        return redirect(url_for('search.index'))
-
-    # Checking if release group is already matched
-    spotify_mapping = mbspotify.mapping([str(release_group_id)])
-    if len(spotify_mapping) > 0:
-        flash(gettext("Thanks, but this album is already matched to Spotify!"))
+    if request.method == 'POST':
+        mbspotify.add_mapping(release_group_id, 'spotify:album:%s' % spotify_id, current_user.id)
+        flash(gettext("Spotify mapping has been added!"), 'success')
         return redirect(url_for('release_group.entity', id=release_group_id))
 
-    spotify_uri = request.args.get('spotify_uri', default=None)
-    if not spotify_uri:
-        return redirect(url_for('.spotify', release_group_id=release_group_id))
-    mbspotify.add_mapping(release_group_id, spotify_uri, current_user.id)
-    flash(gettext('Connection is added!'), 'success')
-    return redirect(url_for('release_group.entity', id=release_group_id))
+    return render_template('matching/confirm.html', release_group=release_group, spotify_album=album)
 
 
 @matching_bp.route('/spotify/<uuid:release_group_id>/report', methods=['POST'], endpoint='spotify_report')
@@ -104,3 +89,15 @@ def spotify_matching_report_handler(release_group_id):
 
     mbspotify.vote(release_group_id, current_user.id)
     return jsonify(success=True)
+
+
+def parse_spotify_id(spotify_ref):
+    # Spotify URI
+    if spotify_ref.startswith('spotify:album:'):
+        return spotify_ref[14:]
+
+    # Link to Spotify
+    if spotify_ref.startswith('http://') or spotify_ref.startswith('https://'):
+        if spotify_ref.endswith('/'):
+            spotify_ref = spotify_ref[:-1]
+        return os.path.split(urlparse(spotify_ref).path)[-1]
