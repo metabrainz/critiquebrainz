60d3278b17d8e313be0bae1e48b4a5423ebddae9
Roman Tsukanov <roman@tsukanov.me>
2014-08-18 07:27:36 +1000

    Documentation updates.

--- fabfile.py
+++ fabfile.py
@@ -9,9 +9,14 @@ def extract_strings():
 
 
 def pull_translations():
-    """Pull translations for languages defined in config from Transifex and compile them."""
+    """Pull translations for languages defined in config from Transifex and compile them.
+
+    Before using this command make sure that you properly configured Transifex client.
+    More info about that is available at http://docs.transifex.com/developer/client/setup#configuration.
+    """
     languages = ','.join(create_app().config['SUPPORTED_LANGUAGES'])
     local("tx pull -f -r critiquebrainz.critiquebrainz -l %s" % languages)
+    print(green("Translations have been updated successfully.", bold=True))
 
 
 def update_strings():
@@ -23,12 +28,18 @@ def update_strings():
 def compile_translations():
     """Compile translations for use."""
     local("pybabel compile -d critiquebrainz/frontend/translations")
+    print(green("Translated strings have been compiled and ready to be used.", bold=True))
 
 
 def compile_styling():
-    """Compile styles.less into styles.css."""
+    """Compile styles.less into styles.css.
+
+    This command requires Less (CSS pre-processor). More information about it can be
+    found at http://lesscss.org/.
+    """
     style_path = "critiquebrainz/frontend/static/css/"
     local("lessc --clean-css %sstyles.less > %sstyles.css" % (style_path, style_path))
+    print(green("Style sheets have been compiled successfully.", bold=True))
 
 
 def deploy():
