eef4b97afe924218827999c6defbcd630889ab58
Roman Tsukanov <roman@tsukanov.me>
2014-08-18 18:38:56 +1000

    Added tests for SpamReport model.

--- critiquebrainz/data/model/report.py
+++ critiquebrainz/data/model/report.py
@@ -10,12 +10,6 @@ class SpamReport(db.Model):
     revision_id = db.Column(db.Integer, db.ForeignKey('revision.id', ondelete='CASCADE'), primary_key=True)
     reported_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def to_dict(self):
-        response = dict(user_id=self.user_id,
-                        review_id=self.review_id,
-                        reported_at=self.reported_at)
-        return response
-
     @classmethod
     def create(cls, revision_id, user):
         report = cls(user=user, revision_id=revision_id)
