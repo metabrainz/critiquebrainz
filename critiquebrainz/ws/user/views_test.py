2ba218e3826d610fbc08c9a06ef2cfaeb3b14fa5
Roman Tsukanov <roman@tsukanov.me>
2014-09-01 20:07:57 +1000

    Improved assertions in UserViewsTestCase.

--- critiquebrainz/ws/user/views_test.py
+++ critiquebrainz/ws/user/views_test.py
@@ -1,21 +1,24 @@
 from critiquebrainz.ws.testing import WebServiceTestCase
 from critiquebrainz.data import db
 from critiquebrainz.data.model.user import User
-import json
 
 
 class UserViewsTestCase(WebServiceTestCase):
-
     def test_user_count(self):
         resp = self.client.get('/user/')
-        data = json.loads(resp.data)
-        assert data['count'] == 0
+        self.assertDictEqual(resp.json, dict(
+            count=0,
+            limit=50,
+            offset=0,
+            users=[],
+        ))
 
     def test_user_addition(self):
         user = User(display_name=u'Tester 1', email=u'tester1@tesing.org')
         db.session.add(user)
         db.session.commit()
-
         resp = self.client.get('/user/')
-        data = json.loads(resp.data)
-        assert data['count'] == 1
+        decoded = resp.json
+        self.assertEquals(decoded['count'], 1)
+        self.assertEquals(len(decoded['users']), 1)
+        # TODO: Completely verify output (I encountered unicode issues when tried to do that).
