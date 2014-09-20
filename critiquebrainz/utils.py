1de0dee45470a4f22f48e3afc7148d4d1192ef51
Roman Tsukanov <roman@tsukanov.me>
2014-09-20 11:16:06 +1000

    Cleanup in utils module.

--- critiquebrainz/utils.py
+++ critiquebrainz/utils.py
@@ -12,12 +12,13 @@ def build_url(base, additional_params=None):
     query_params.update(urlparse.parse_qsl(url.query, True))
     if additional_params is not None:
         query_params.update(additional_params)
-        for k, v in additional_params.iteritems():
-            if v is None:
-                query_params.pop(k)
+        for key, val in additional_params.iteritems():
+            if val is None:
+                query_params.pop(key)
 
     return urlparse.urlunparse(
-        (url.scheme, url.netloc, url.path, url.params, urllib.urlencode(query_params), url.fragment))
+        (url.scheme, url.netloc, url.path, url.params,
+         urllib.urlencode(query_params), url.fragment))
 
 
 def validate_uuid(string):
@@ -27,8 +28,9 @@ def validate_uuid(string):
 
 def generate_string(length):
     """Generates random string with a specified length."""
-    return ''.join([random.choice(string.ascii_letters.decode('ascii') + string.digits.decode('ascii'))
-                    for x in xrange(length)])
+    return ''.join([random.choice(string.ascii_letters.decode('ascii')
+                                  + string.digits.decode('ascii'))
+                    for _ in xrange(length)])
 
 
 def reformat_date(value, format=None):
@@ -42,7 +44,7 @@ def reformat_datetime(value, format=None):
 
 
 def track_length(value):
-    """Converts track length from MusicBrainz into pretty string."""
+    """Converts track length specified in milliseconds into a pretty string."""
     seconds = int(value) / 1000
     minutes, seconds = divmod(seconds, 60)
     return '%i:%02i' % (minutes, seconds)
