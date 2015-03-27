4279b5532ea986c0c3488f1740f0fe546ee76ddd
Shadab Zafar <dufferzafar0@gmail.com>
2015-03-27 16:58:51 +0530

    Fixed some issues

--- critiquebrainz/ws/review/views.py
+++ critiquebrainz/ws/review/views.py
@@ -133,15 +133,15 @@ def review_post_handler(user):
     :json string text: review contents, min length is 25, max is 5000
     :json string license_choice: license ID
     :json string lang: language code (ISO 639-1), default is ``en`` **(optional)**
-    :json boolean is_draft: whether the review should be saved as a draft or not
+    :json boolean is_draft: whether the review should be saved as a draft or not, default is ``False`` **(optional)**
 
     :resheader Content-Type: *application/json*
     """
 
     def fetch_params():
-        is_draft = Parser.bool('json', 'is_draft')
+        is_draft = Parser.bool('json', 'is_draft', optional=True) or False
         if is_draft:
-            REVIEW_MIN_LENGTH = REVIEW_MAX_LENGTH = None
+            REVIEW_MIN_LENGTH = None
         release_group = Parser.uuid('json', 'release_group')
         text = Parser.string('json', 'text', min=REVIEW_MIN_LENGTH, max=REVIEW_MAX_LENGTH)
         license_choice = Parser.string('json', 'license_choice')
