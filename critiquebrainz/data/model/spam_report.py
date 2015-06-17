0664af9e11f16b2f9d6fca70ebe79d142e89bccb
Shadab Zafar <dufferzafar0@gmail.com>
2015-06-17 16:15:05 +0530

    Add reason attribute to spam reports

--- critiquebrainz/data/model/spam_report.py
+++ critiquebrainz/data/model/spam_report.py
@@ -12,12 +12,13 @@ class SpamReport(db.Model, DeleteMixin):
     __tablename__ = 'spam_report'
 
     user_id = db.Column(UUID, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
+    reason = db.Column(db.Unicode)
     revision_id = db.Column(db.Integer, db.ForeignKey('revision.id', ondelete='CASCADE'), primary_key=True)
     reported_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
     @classmethod
-    def create(cls, revision_id, user):
-        report = cls(user=user, revision_id=revision_id)
+    def create(cls, revision_id, user, reason):
+        report = cls(user=user, revision_id=revision_id, reason=reason)
         db.session.add(report)
         db.session.commit()
         return report
