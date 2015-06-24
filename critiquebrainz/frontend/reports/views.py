77ea5a503b68d690c0ce49c081c8556cbc4451bb
Shadab Zafar <dufferzafar0@gmail.com>
2015-06-24 16:30:11 +0530

    Reports list is now paginated, 20 per page

--- critiquebrainz/frontend/reports/views.py
+++ critiquebrainz/frontend/reports/views.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
 from flask_login import login_required, current_user
 from werkzeug.exceptions import Unauthorized
 from critiquebrainz.data.model.spam_report import SpamReport
+from sqlalchemy import desc
 
 reports_bp = Blueprint('reports', __name__)
+
+RESULTS_LIMIT = 20
 
 
 @reports_bp.route('/')
@@ -11,6 +14,24 @@ reports_bp = Blueprint('reports', __name__)
 def index():
     if not current_user.is_admin():
         raise Unauthorized('You must be an administrator to view this page.')
-    reports, count = SpamReport.list()
-    return render_template('reports/reports.html', reports=reports)
+
+    count = SpamReport.query.count()
+    results = SpamReport.query.order_by(desc(SpamReport.reported_at)).limit(RESULTS_LIMIT)
+
+    return render_template('reports/reports.html', count=count, results=results, limit=RESULTS_LIMIT)
+
+
+@reports_bp.route('/more')
+def more():
+    if not current_user.is_admin():
+        raise Unauthorized('You must be an administrator to view this page.')
+
+    page = int(request.args.get('page', default=0))
+    offset = page * RESULTS_LIMIT
+
+    count = SpamReport.query.count()
+    results = SpamReport.query.order_by(desc(SpamReport.reported_at)).offset(offset).limit(RESULTS_LIMIT)
+
+    template = render_template('reports/reports_results.html', results=results)
+    return jsonify(results=template, more=(count-offset-RESULTS_LIMIT) > 0)
 
