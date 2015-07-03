d1ac894fb01694ee5a75088e6154d47ebda7fa30
Shadab Zafar <dufferzafar0@gmail.com>
2015-07-03 11:56:33 +0530

    Return 404 when there are no logs to display

--- critiquebrainz/frontend/log/views.py
+++ critiquebrainz/frontend/log/views.py
@@ -1,6 +1,8 @@
 from itertools import groupby
 from flask import Blueprint, render_template
+from flask_babel import gettext
 from critiquebrainz.data.model.admin_log import AdminLog
+from werkzeug.exceptions import NotFound
 
 log_bp = Blueprint('log', __name__)
 
@@ -10,6 +12,9 @@ RESULTS_LIMIT = 20
 @log_bp.route('/')
 def browse():
     results, count = AdminLog.list()
+
+    if not results:
+        raise NotFound(gettext("No logs to display."))
 
     results = groupby(results, lambda log: log.timestamp.strftime('%d %b, %G'))
 
