944dd9ef6d25680287a610152435cc9d0761622b
Shadab Zafar <dufferzafar0@gmail.com>
2015-07-16 21:27:12 +0530

    Add pagination views to admin log

--- critiquebrainz/frontend/log/views.py
+++ critiquebrainz/frontend/log/views.py
@@ -1,7 +1,8 @@
 from itertools import groupby
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request, jsonify
 from flask_babel import gettext
 from critiquebrainz.data.model.admin_log import AdminLog
+from sqlalchemy import desc
 
 log_bp = Blueprint('log', __name__)
 
@@ -10,11 +11,25 @@ RESULTS_LIMIT = 20
 
 @log_bp.route('/')
 def browse():
-    results, count = AdminLog.list()
+    count = AdminLog.query.count()
+    results = AdminLog.query.order_by(desc(AdminLog.timestamp)).limit(RESULTS_LIMIT)
 
     if not results:
         flash(gettext("No logs to display."), "warning")
 
     results = groupby(results, lambda log: log.timestamp.strftime('%d %b, %G'))
 
-    return render_template('log/log.html', results=results)
+    return render_template('log/log.html', count=count, results=results, limit=RESULTS_LIMIT)
+
+
+@log_bp.route('/more')
+def more():
+    page = int(request.args.get('page', default=0))
+    offset = page * RESULTS_LIMIT
+
+    count = AdminLog.query.count()
+    results = AdminLog.query.order_by(desc(AdminLog.timestamp)).offset(offset).limit(RESULTS_LIMIT)
+    results = groupby(results, lambda log: log.timestamp.strftime('%d %b, %G'))
+
+    template = render_template('log/log_results.html', results=results)
+    return jsonify(results=template, more=(count-offset-RESULTS_LIMIT) > 0)
