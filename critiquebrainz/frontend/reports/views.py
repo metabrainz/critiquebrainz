ae40ed5720eee7977faa2a76f96d07faf90b04e7
Shadab Zafar <dufferzafar0@gmail.com>
2015-07-17 17:58:21 +0530

    Use the report list function rather than sql query

--- critiquebrainz/frontend/reports/views.py
+++ critiquebrainz/frontend/reports/views.py
@@ -4,7 +4,6 @@ from flask_babel import gettext
 from werkzeug.exceptions import NotFound
 from critiquebrainz.data.model.spam_report import SpamReport
 from critiquebrainz.frontend.login import admin_view
-from sqlalchemy import desc
 
 reports_bp = Blueprint('reports', __name__)
 
@@ -16,7 +15,7 @@ RESULTS_LIMIT = 20
 @admin_view
 def index():
     count = SpamReport.query.count()
-    results = SpamReport.query.order_by(desc(SpamReport.reported_at)).limit(RESULTS_LIMIT)
+    results, _ = SpamReport.list(limit=RESULTS_LIMIT)
     return render_template('reports/reports.html', count=count, results=results, limit=RESULTS_LIMIT)
 
 
@@ -28,7 +27,7 @@ def more():
     offset = page * RESULTS_LIMIT
 
     count = SpamReport.query.count()
-    results = SpamReport.query.order_by(desc(SpamReport.reported_at)).offset(offset).limit(RESULTS_LIMIT)
+    results, _ = SpamReport.list(offset=offset, limit=RESULTS_LIMIT)
 
     template = render_template('reports/reports_results.html', results=results)
     return jsonify(results=template, more=(count-offset-RESULTS_LIMIT) > 0)
