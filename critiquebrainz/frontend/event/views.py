51b6b27adbe8e3102fd3f8a67093fd4e5c2cc0a1
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-12 14:13:28 +0530

    Display associated reviews on event page

--- critiquebrainz/frontend/event/views.py
+++ critiquebrainz/frontend/event/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import current_user
 from flask_babel import gettext
 from critiquebrainz.frontend.apis import musicbrainz
+from critiquebrainz.data.model.review import Review
 from werkzeug.exceptions import NotFound
 
 
@@ -9,9 +11,23 @@ event_bp = Blueprint('event', __name__)
 
 @event_bp.route('/<uuid:id>')
 def entity(id):
+    id = str(id)
     event = musicbrainz.get_event_by_id(id)
     if not event:
         raise NotFound(gettext("Sorry, we couldn't find a event with that MusicBrainz ID."))
 
-    reviews = []
-    return render_template('event.html', id=id, event=event, reviews=reviews)
+    if current_user.is_authenticated():
+        my_reviews, my_count = Review.list(entity_id=id, entity_type='event', user_id=current_user.id)
+        if my_count != 0:
+            my_review = my_reviews[0]
+        else:
+            my_review = None
+    else:
+        my_review = None
+
+    limit = int(request.args.get('limit', default=10))
+    offset = int(request.args.get('offset', default=0))
+    reviews, count = Review.list(entity_id=id, entity_type='event', sort='rating', limit=limit, offset=offset)
+
+    return render_template('event.html', id=id, event=event, reviews=reviews,
+                           my_review=my_review, limit=limit, offset=offset, count=count)
