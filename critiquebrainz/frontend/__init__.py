edeaac1cad66291586255d698cab834c121a97a5
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-22 18:20:24 +0300

    Display review language name instead of code
    This commit defines a custom Jinja2 filter that uses Babel's Locale
    class to get the localized language name for a language code. This
    filter is then used when displayign reviews' languages.

--- critiquebrainz/frontend/__init__.py
+++ critiquebrainz/frontend/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from Babel import Locale
+
 import os
 
 
@@ -70,6 +72,7 @@ def create_app(debug=None):
     app.jinja_env.filters['datetime'] = reformat_datetime
     app.jinja_env.filters['track_length'] = track_length
     app.jinja_env.filters['entity_details'] = musicbrainz.get_entity_by_id
+    app.jinja_env.filters['language_name'] = lambda language_code: Locale(language_code).language_name
 
     # Blueprints
     from critiquebrainz.frontend.views import frontend_bp
