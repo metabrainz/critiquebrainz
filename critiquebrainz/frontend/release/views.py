0472d87ac2754d96cc1be0cd5ca57bd8aa7da702
Ohm Patel <ohmjpatel99@gmail.com>
2015-12-22 19:11:39 -0600

    Redundancy fixes

--- critiquebrainz/frontend/release/views.py
+++ critiquebrainz/frontend/release/views.py
@@ -8,8 +8,8 @@ release_bp = Blueprint('release', __name__)
 @release_bp.route('/<uuid:id>')
 def entity(id):
     id = str(id)
-    if musicbrainz.get_release_by_id(id):
-        release_data = musicbrainz.get_release_by_id(id)
+    release_data = musicbrainz.get_release_by_id(id)
+    if release_data:
         group_id = release_data['release-group']['id']
         url = '/release-group/' + str(group_id)
         return redirect(url, 301)
