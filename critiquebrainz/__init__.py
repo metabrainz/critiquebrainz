b95b737a418279cf49fc2a961b980a1a8b57fab0
Roman Tsukanov <roman@tsukanov.me>
2016-04-12 08:05:50 +0200

    Use separate app creation function for WS docs

--- critiquebrainz/__init__.py
+++ critiquebrainz/__init__.py
@@ -7,12 +7,3 @@ try:
     urllib3.contrib.pyopenssl.inject_into_urllib3()
 except ImportError:
     pass
-
-# Config file creation for ReadTheDocs
-# See http://read-the-docs.readthedocs.org/en/latest/faq.html#how-do-i-change-behavior-for-read-the-docs
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    import shutil
-    path = os.path.dirname(__file__)
-    shutil.copyfile(path+'/config.py.example', path+'/config.py')
