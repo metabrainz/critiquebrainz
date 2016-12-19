f2904e94e885d2afa63e2df5ea6e92e7cd382246
Roman Tsukanov <roman@tsukanov.me>
2016-12-19 05:41:07 +0100

    Fix the case where there might be no releases to extract URLs from

--- critiquebrainz/frontend/external/soundcloud.py
+++ critiquebrainz/frontend/external/soundcloud.py
@@ -3,10 +3,11 @@ import re
 
 
 def get_url(mbid):
-    all_url_rels = musicbrainz.get_url_rels_from_releases(musicbrainz.browse_releases(release_group=mbid,
-                                                                                      includes=['url-rels']))
+    all_url_rels = musicbrainz.get_url_rels_from_releases(
+        musicbrainz.browse_releases(release_group=mbid, includes=['url-rels']) or []
+    )
     for url_rel in all_url_rels:
-        if url_rel['type-id'] == '08445ccf-7b99-4438-9f9a-fb9ac18099ee':  # Type-id for streaming music
+        if url_rel['type-id'] == '08445ccf-7b99-4438-9f9a-fb9ac18099ee':  # "streaming music"
             if re.match(r'^(http|https)://soundcloud.com', url_rel['target']):
                 return url_rel['target']
     return None
