9c12be51ff7ece6f7448700f68b51dd3433c70e4
Suyash Garg <suyashgargsfam@gmail.com>
2017-02-12 11:51:06 +0530

    CB-230: Functions and tests to migrate user code
    Functions and Tests for migrating users code off the ORM

--- critiquebrainz/db/users.py
+++ critiquebrainz/db/users.py
@@ -1,6 +1,9 @@
 from hashlib import md5
+import critiquebrainz.db.revision as db_revision
 import sqlalchemy
 from critiquebrainz import db
+from datetime import datetime
+import uuid
 
 
 def gravatar_url(source, default="identicon", rating="pg"):
@@ -70,3 +73,543 @@ def get_many_by_mb_username(usernames):
             else:
                 user["avatar_url"] = gravatar_url(default_gravatar_src)
         return users
+
+
+def get_by_id(user_id):
+    """Get user from user_id
+
+    Args:
+        user_id(uuid): ID of the user
+
+    Returns:
+        Dictionary with the following structure:
+        {
+            "id": (uuid)
+            "display_name": (str)
+            "email": (str)
+            "created": (datetime)
+            "musicbrainz_id": (str)
+            "show_gravatar": (bool)
+            "is_blocked": (bool)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT id, display_name, email, created,
+                   musicbrainz_id, show_gravatar, is_blocked
+              FROM "user"
+             WHERE id = :user_id
+        """), {
+            "user_id": user_id
+        })
+        row = result.fetchone()
+    return dict(row) if row else None
+
+
+def create(display_name, **kwargs):
+    """Create user using the given details
+
+    Args:
+        display_name(str): display_name of the user,
+        musicbrainz_id(str, optional): musicbrainz_id of user,
+        email(str, optional): email of the user,
+        show_gravatar(bool, optional): whether to show gravatar(default: false)
+        is_blocked(bool, optional): whether user is blocked(default: false)
+
+    Returns:
+        Dictionary with the following structure:
+        {
+            "id": (uuid)
+            "display_name": (str)
+            "email": (str)
+            "created": (datetime)
+            "musicbrainz_id": (str)
+            "show_gravatar": (bool)
+            "is_blocked": (bool)
+        }
+    """
+    id = str(uuid.uuid4())
+    musicbrainz_id = kwargs.pop('musicbrainz_id', None)
+    email = kwargs.pop('email', None)
+    show_gravatar = kwargs.pop('show_gravatar', False)
+    is_blocked = kwargs.pop('is_blocked', False)
+    if(kwargs):
+        raise TypeError('Unexpected **kwargs: %r' % kwargs)
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            INSERT INTO "user"
+                 VALUES (:id, :display_name, :email, :created, :musicbrainz_id, :show_gravatar, :is_blocked)
+              RETURNING id
+            """), {
+                "id": id,
+                "display_name": display_name,
+                "email": email,
+                "created": datetime.now(),
+                "musicbrainz_id": musicbrainz_id,
+                "show_gravatar": show_gravatar,
+                "is_blocked": is_blocked
+            })
+        new_id = result.fetchone()[0]
+        user = get_by_id(new_id)
+    return user
+
+
+def get_by_mbid(musicbrainz_id):
+    """Get user by musicbrainz_id
+
+    Args:
+        musicbrainz_id(str): Musicbrainz ID of the User
+
+    Returns:
+        Dictionary with the following structure:
+        {
+            "id": (uuid)
+            "display_name": (str)
+            "email": (str)
+            "created": (datetime)
+            "musicbrainz_id": (str)
+            "show_gravatar": (bool)
+            "is_blocked": (bool)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT id, display_name, email, created,
+                   musicbrainz_id, show_gravatar, is_blocked
+            FROM "user"
+            WHERE musicbrainz_id = :musicbrainz_id
+        """), {
+            "musicbrainz_id": musicbrainz_id
+        })
+        row = result.fetchone()
+    return dict(row) if row else None
+
+
+def get_or_create(display_name, musicbrainz_id, **kwargs):
+    """Get user from display_name and musicbrainz_id
+
+    Args:
+        display_name(str): display_name of the user
+        musicbrainz_id(str): Musicbrainz ID of the user
+        email(str, optional): email of the user,
+        show_gravatar(bool, optional): whether to show gravatar(default: false)
+        is_blocked(bool, optional): whether user is blocked(default: false)
+
+    Returns:
+        Dictionary with the following structure:
+        {
+            "id": (uuid)
+            "display_name": (str)
+            "email": (str)
+            "created": (datetime)
+            "musicbrainz_id": (str)
+            "show_gravatar": (bool)
+            "is_blocked": (bool)
+        }
+    """
+    user = get_by_mbid(musicbrainz_id)
+    if not user:
+        user = create(display_name, musicbrainz_id=musicbrainz_id, **kwargs)
+    return user
+
+
+def get_count():
+    """Returns the total number of users
+
+    Returns:
+        count: (int)
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT count(*)
+              FROM "user"
+        """))
+        count = result.fetchone()[0]
+    return count
+
+
+def list(limit=None, offset=0):
+    """Returns the list of users of critiquebrainz
+
+    Args:
+        limit(int, optional): Number of users to be returned
+        offset(int, optional): Number of initial users to skip
+
+    Returns:
+        List of dictionaries of users with the following structure:
+        {
+            "id": (uuid)
+            "display_name": (str)
+            "email": (str)
+            "created": (datetime)
+            "musicbrainz_id": (str)
+            "show_gravatar": (bool)
+            "is_blocked": (bool)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT id, display_name, email, created,
+                   musicbrainz_id, show_gravatar, is_blocked
+              FROM "user"
+             LIMIT :limit
+            OFFSET :offset
+        """), {
+            "limit": limit,
+            "offset": offset
+        })
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+    return rows
+
+
+def unblock(user_id):
+    """Unblock user (admin only)
+
+    Args:
+        user_id(uuid): ID of user to be unblocked
+    """
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            UPDATE "user"
+               SET is_blocked = 'false'
+             WHERE id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+
+def block(user_id):
+    """Block user (admin only)
+
+    Args:
+        user_id(uuid): ID of user to be blocked
+    """
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            UPDATE "user"
+               SET is_blocked = 'true'
+            WHERE id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+
+def has_voted(user_id, review_id):
+    """Check if a user has already voted a review
+
+    Args:
+        user_id(uuid): ID of the user
+        review_id(uuid): ID of the review
+
+    Returns:
+        (bool): True if has voted else False
+    """
+    last_revision = db_revision.get(review_id, limit=1)[0]
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT count(*)
+              FROM vote
+             WHERE revision_id = :revision_id
+               AND user_id = :user_id
+            """), {
+                "revision_id": last_revision['id'],
+                "user_id": user_id
+            })
+        count = result.fetchone()[0]
+    return count > 0
+
+
+def karma(user_id):
+    """Get the karma of a user
+
+    Args:
+        user_id(uuid): ID of the user
+
+    Returns:
+        karma(int): the karma of the user
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT review.user_id, vote
+              FROM ((vote
+         LEFT JOIN revision
+                ON revision.id = revision_id)
+         LEFT JOIN review
+                ON review.id = review_id)
+         LEFT JOIN "user"
+                ON "user".id = review.user_id
+             WHERE "user".id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+        rows = result.fetchall()
+        _karma = 0
+        for row in rows:
+            if row.vote == True:
+                _karma += 1
+            else:
+                _karma -= 1
+    return _karma
+
+
+def reviews(user_id):
+    """Get list of reviews written by a user
+
+    Args:
+        user_id(uuid): ID of the user
+
+    Returns:
+        List of reviews written by the user with the structure:
+        {
+            "id": (uuid)
+            "entity_id": (uuid),
+            "entity_type": event or place or release_group,
+            "user_id": (uuid),
+            "edits": (int),
+            "is_draft": (bool),
+            "license_id": (str),
+            "language": (str),
+            "source": (str),
+            "source_url" (str)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT id, entity_id, entity_type, user_id, edits,
+                   is_draft, license_id, language, source, source_url
+              FROM review
+             WHERE user_id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+
+    return rows
+
+
+def get_votes(user_id, date='1-1-1970'):
+    """Get votes by a user from a specified time
+
+    Args:
+        user_id(uuid): ID of the user
+        date(datetime): Date from which votes submitted(default: UTC)
+
+    Returns:
+        List of votes submitted by the user from the time specified
+        {
+            "vote": (bool),
+            "rated_at": (datetime)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT vote, rated_at
+              FROM vote
+             WHERE user_id = :user_id
+               AND rated_at >= :date
+        """), {
+            "user_id": user_id,
+            "date": date
+        })
+
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+
+    return rows
+
+
+def get_reviews(user_id, date='1-1-1970'):
+    """Get reviews by a user from a specified time
+
+    Args:
+        user_id(uuid): ID of the user,
+        date(datetime): Date from which reviews submitted(default: UTC)
+
+    Returns:
+        List of reviews by the user from the time specified
+        {
+            "id": (uuid)
+            "entity_id": (uuid),
+            "entity_type": event or place or release_group,
+            "user_id": (uuid),
+            "edits": (int),
+            "is_draft": (bool),
+            "license_id": (str),
+            "language": (str),
+            "source": (str),
+            "source_url" (str)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+           SELECT *
+           FROM review
+      LEFT JOIN
+        (SELECT review_id
+              , min(timestamp)
+             AS creation_time
+           FROM revision
+       GROUP BY review_id)
+             AS review_create
+             ON review.id = review_id
+          WHERE user_id = :user_id
+            AND creation_time > :date
+        """), {
+            "user_id": user_id,
+            "date": date
+        })
+
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+
+    return rows
+
+
+def update(user, display_name=None, email=None, show_gravatar=None):
+    """Update info of a user
+
+    Args:
+        user: User object whose info is to be updated,
+        display_name(str): Display name of user,
+        email(str): Email of the user,
+        show_gravatar(bool): whether to show gravatar
+    """
+    if display_name is None:
+        display_name = user.display_name
+    if show_gravatar is None:
+        show_gravatar = user.show_gravatar
+    if email is None:
+        email = user.email
+
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            UPDATE "user"
+               SET display_name = :display_name,
+                  show_gravatar = :show_gravatar,
+                          email = :email
+             WHERE id = :user_id
+        """), {
+            "user_id": user.id,
+            "display_name": display_name,
+            "show_gravatar": show_gravatar,
+            "email": email
+        })
+
+
+def delete(user_id):
+    """Delete a user
+    Deletes user, votes, reviews, revisions and spam reports
+    by it
+
+    Args:
+        user_id(uuid): ID of the user to be deleted
+    """
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            DELETE
+              FROM "user"
+             WHERE id = :user_id
+        """), {
+            "user_id": user_id
+        })
+        connection.execute(sqlalchemy.text("""
+            DELETE
+              FROM "vote"
+             WHERE user_id = :user_id
+        """), {
+            "user_id": user_id
+        })
+        connection.execute(sqlalchemy.text("""
+            DELETE
+              FROM "spam_report"
+             WHERE user_id = :user_id
+        """), {
+            "user_id": user_id
+        })
+    user_reviews = reviews(user_id)
+    with db.engine.connect() as connection:
+        for review in user_reviews:
+            connection.execute(sqlalchemy.text("""
+                DELETE
+                  FROM review
+                 WHERE id = :review_id
+            """), {
+                "review_id": review['id']
+            })
+            connection.execute(sqlalchemy.text("""
+                DELETE
+                  FROM revision
+                 WHERE review_id = :review_id
+            """), {
+                "review_id": review['id']
+            })
+
+
+def clients(user_id):
+    """Get list of oauth clients registered by user
+
+    Args:
+        user_id(uuid): ID of the user,
+
+    Returns:
+        List of dictionaries of oauth clients of the user
+        {
+            "client_id": (unicode),
+            "client_secret": (unicode),
+            "redirect_uri": (unicode),
+            "user_id": (uuid),
+            "name": (unicode),
+            "desc": (unicode),
+            "website": (unicode)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT client_id, client_secret, redirect_uri,
+                   user_id, name, oauth_client.desc, website
+              FROM oauth_client
+             WHERE user_id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+    return rows
+
+
+def tokens(user_id):
+    """Get the oauth_tokens from a user_id
+
+    Args:
+        user_id(uuid): ID of the user
+
+    Returns:
+        List of dictionaries of client tokens of the user
+        {
+            "id": (int),
+            "client_id": (Unicode),
+            "access_token": (Unicode),
+            "refresh_token": (Unicode),
+            "expires": (datetime),
+            "scopes": (Unicode)
+        }
+    """
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT id, client_id, access_token,
+                   refresh_token, expires, scopes
+              FROM oauth_token
+             WHERE user_id = :user_id
+        """), {
+            "user_id": user_id
+        })
+
+        rows = result.fetchall()
+        rows = [dict(row) for row in rows]
+    return rows
--- critiquebrainz/db/users_test.py
+++ critiquebrainz/db/users_test.py
@@ -1,13 +1,34 @@
 from critiquebrainz.data.testing import DataTestCase
-from critiquebrainz.data.model.user import User
+from critiquebrainz.db.user import User
+from critiquebrainz.data import db
+import critiquebrainz.db.users as db_users
 from critiquebrainz.db.users import gravatar_url, get_many_by_mb_username
-
+from critiquebrainz.data.model.review import Review
+from critiquebrainz.data.model.spam_report import SpamReport
+from critiquebrainz.data.model.license import License
+import critiquebrainz.db.vote as db_vote
+from datetime import datetime, date, timedelta
+from uuid import UUID
 
 class UserTestCase(DataTestCase):
     def setUp(self):
         super(UserTestCase, self).setUp()
-        self.user1 = User.get_or_create("test", musicbrainz_id='tester_1')
-        self.user2 = User.get_or_create("test1", musicbrainz_id="тестер")
+        self.user1 = User(db_users.get_or_create("test", musicbrainz_id='tester_1'))
+        self.user2 = User(db_users.get_or_create("test1", musicbrainz_id="тестер"))
+        self.author = User(db_users.get_or_create("Author1", musicbrainz_id="author1"))
+        license = License(id='Test', full_name='Test License')
+        db.session.add(license)
+        db.session.commit()
+        self.review = Review.create(release_group='e7aad618-fa86-3983-9e77-405e21796eca',
+                               text="Testing!",
+                               user_id=self.author.id,
+                               is_draft=False,
+                               license_id=license.id)
+        vote = db_vote.submit(self.user1.id, self.review.last_revision.id, True)
+        self.review_created = self.review.last_revision.timestamp
+        self.review_id = self.review.id
+        self.revision_id = self.review.last_revision.id
+        self.report = SpamReport.create(self.revision_id, self.user1.id, "Testing Reason Report")
 
     def test_get_many_by_mb_username(self):
         users = [self.user1.musicbrainz_id, self.user2.musicbrainz_id]
@@ -21,3 +42,95 @@ class UserTestCase(DataTestCase):
             gravatar_url("test2"),
             "https://gravatar.com/avatar/ad0234829205b9033196ba818f7a872b?d=identicon&r=pg"
         )
+
+    def test_get_by_id(self):
+        user = db_users.get_by_id(self.user1.id)
+        self.assertEqual(user['display_name'], "test")
+        self.assertEqual(user['musicbrainz_id'], "tester_1")
+
+    def test_users_list(self):
+        users = db_users.list()
+        self.assertEqual(len(users), 3)
+
+        users = db_users.list(0, 10)
+        self.assertEqual(len(users), 0)
+
+        user3 = db_users.get_or_create("test2", "user_1")
+        users = db_users.list(1, 1)
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0]['display_name'], "test1")
+
+    def test_get_count(self):
+        count = db_users.get_count()
+        self.assertEqual(count, 3)
+
+        user3 = db_users.get_or_create("test2", "user_1")
+        count = db_users.get_count()
+        self.assertEqual(count, 4)
+
+    def test_get_by_mbid(self):
+        user = db_users.get_by_mbid("tester_1")
+        self.assertEqual(user['display_name'], "test")
+
+    def test_create(self):
+        user = db_users.create("test_user", email="foo@foo.com", musicbrainz_id="tester2")
+        self.assertEqual(user['email'], "foo@foo.com")
+        self.assertEqual(type(user['id']), UUID)
+        self.assertEqual(user['is_blocked'], False)
+
+    def test_vote(self):
+        voted = db_users.has_voted(self.user1.id, self.review.id)
+        self.assertEqual(voted, True)
+        voted = db_users.has_voted(self.user2.id, self.review.id)
+        self.assertEqual(voted, False)
+
+    def test_karma(self):
+        karma = db_users.karma(self.author.id)
+        self.assertEqual(karma, 1)
+        karma = db_users.karma(self.user2.id)
+        self.assertEqual(karma, 0)
+
+    def test_reviews(self):
+        review = db_users.reviews(self.author.id)[0]
+        self.assertEqual(review['license_id'], 'Test')
+        self.assertEqual(type(review['user_id']), UUID)
+        self.assertEqual(str(review['user_id']), self.author.id)
+
+    def test_votes_since(self):
+        votes = db_users.get_votes(self.user1.id)
+        self.assertEqual(len(votes), 1)
+        two_days_from_now = date.today() + timedelta(days=2)
+        votes = db_users.get_votes(self.user1.id, two_days_from_now)
+        self.assertEqual(len(votes), 0)
+
+    def test_review_since(self):
+        reviews = db_users.get_reviews(self.author.id)
+        self.assertEqual(len(reviews), 1)
+        self.review.update(text="Testing Again")
+        reviews = db_users.get_reviews(self.author.id)
+        self.assertEqual(len(reviews), 1)
+        self.assertEqual(reviews[0]['creation_time'], self.review_created)
+        two_days_from_now = date.today() + timedelta(days=2)
+        reviews = db_users.get_reviews(self.author.id, two_days_from_now)
+        self.assertEqual(len(reviews), 0)
+
+    def test_update(self):
+        db_users.update(self.user1, email='foo@foo.com')
+        user1 = db_users.get_by_id(self.user1.id)
+        self.assertEqual(user1['email'], 'foo@foo.com')
+
+    def test_delete(self):
+
+        user1_id = self.user1.id
+        db_users.delete(self.user1.id)
+        # Votes should be deleted as well
+        votes = db_users.get_votes(self.user1.id)
+        self.assertEqual(len(votes), 0)
+        # Spam Reports to be deleted as well
+        spam_report_count = SpamReport.query.filter_by(user_id=user1_id, revision_id=self.revision_id).count()
+        self.assertEqual(spam_report_count, 0)
+
+        db_users.delete(self.author.id)
+        # Review should not exist
+        reviews = db_users.get_reviews(self.author.id)
+        self.assertEqual(len(reviews), 0)
