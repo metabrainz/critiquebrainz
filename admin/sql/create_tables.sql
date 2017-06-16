03de33dc887d1b8f281f20ff3dd249c0476c61e8
Pinank Solanki <solankipinank10@gmail.com>
2017-06-16 23:25:30 +0530

    Implement schema changes to store ratings
    - Add rating column to revision table to store rating
    - Create new table avg_rating to store average rating of an entity

--- admin/sql/create_tables.sql
+++ admin/sql/create_tables.sql
@@ -67,7 +67,17 @@ CREATE TABLE revision (
     id          SERIAL      NOT NULL,
     review_id   UUID,
     "timestamp" TIMESTAMP   NOT NULL,
-    text        VARCHAR     NOT NULL
+    text        VARCHAR,     
+    rating      SMALLINT CHECK (rating >= 0 AND rating <= 100)
+);
+ALTER TABLE revision ADD CONSTRAINT revision_text_rating_both_not_null_together
+    CHECK (rating is NOT NULL OR text is NOT NULL);
+
+CREATE TABLE avg_rating (
+    entity_id   UUID         NOT NULL,
+    entity_type entity_types NOT NULL,
+    rating      SMALLINT     NOT NULL CHECK (rating >= 0 AND rating <= 100),
+    count       INTEGER      NOT NULL
 );
 
 CREATE TABLE spam_report (
