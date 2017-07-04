dc9f9abb2dce482012aef78234d7fd01e9d68f25
Suyash Garg <suyashgargsfam@gmail.com>
2017-07-04 18:37:44 +0530

    CB-231: Use direct MB database access for the place entity

--- critiquebrainz/frontend/external/musicbrainz_db/__init__.py
+++ critiquebrainz/frontend/external/musicbrainz_db/__init__.py
@@ -1,8 +1,21 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.pool import NullPool
+from contextlib import contextmanager
 
 engine = None
 
 def init_db_engine(connect_str):
-    global engine
+    global engine, Session
     engine = create_engine(connect_str, poolclass=NullPool)
+    Session = scoped_session(
+        sessionmaker(bind=engine)
+    )
+
+@contextmanager
+def mb_session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
---
+++ critiquebrainz/frontend/external/musicbrainz_db/includes.py
@@ -0,0 +1,29 @@
+import critiquebrainz.frontend.external.musicbrainz_db.exceptions as mb_exceptions
+
+RELATABLE_TYPES = [
+    'area',
+    'artist',
+    'label',
+    'place',
+    'event',
+    'recording',
+    'release',
+    'release-group',
+    'series',
+    'url',
+    'work',
+    'instrument'
+]
+RELATION_INCLUDES = [entity + '-rels' for entity in RELATABLE_TYPES]
+TAG_INCLUDES = ["tags", "user-tags"]
+RATING_INCLUDES = ["ratings", "user-ratings"]
+VALID_INCLUDES = {
+   'place' : ["aliases", "annotation"] + RELATION_INCLUDES + TAG_INCLUDES,
+}
+
+
+def check_includes(entity, includes):
+    """Check if includes specified for an entity are valid includes."""
+    for include in includes:
+        if include not in VALID_INCLUDES[entity]:
+            raise mb_exceptions.InvalidIncludeError("Bad includes: {inc} is not a valid include".format(inc=include))
