573608a5d29b6c2abe3a9cfb1dbe78f7dea340ff
Pinank Solanki <solankipinank10@gmail.com>
2017-07-18 23:20:39 +0530

    Added IntegrityError in db/exceptions

--- critiquebrainz/db/exceptions.py
+++ critiquebrainz/db/exceptions.py
@@ -11,3 +11,7 @@ class NoDataFoundException(DatabaseException):
 class BadDataException(DatabaseException):
     """Should be used when incorrect data is being submitted."""
     pass
+
+class IntegrityError(DatabaseException):
+	"""Should be used when any database constraint is violated"""
+	pass
