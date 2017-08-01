94e19a4abe57621cc10c1e041c37bfade14fa4f1
Suyash Garg <suyashgargsfam@gmail.com>
2017-08-01 14:34:25 +0530

    Add docstring for get_entities_by_gids, update exception

--- critiquebrainz/frontend/external/musicbrainz_db/utils.py
+++ critiquebrainz/frontend/external/musicbrainz_db/utils.py
@@ -2,6 +2,17 @@ import critiquebrainz.frontend.external.musicbrainz_db.exceptions as mb_exceptio
 
 
 def get_entities_by_gids(query, entity_model, redirect_model, mbids):
+    """Get entities using their mbids.
+
+    Args:
+        query (Query): sqlalchemy Query object.
+        entity_model (mbdata.models): Model of the target entity.
+        redirect_model (mbdata.models): Redirect Model of the target entity.
+        mbids (list): IDs of the target entities.
+
+    Returns:
+        List of objects of target entities.
+    """
     entities = query.filter(entity_model.gid.in_(mbids)).all()
     remaining_gids = list(set(mbids) - {entity.gid for entity in entities})
     if remaining_gids:
@@ -13,5 +24,5 @@ def get_entities_by_gids(query, entity_model, redirect_model, mbids):
         entities.extend(redirected_entities)
     remaining_gids = list(set(mbids) - {entity.gid for entity in entities})
     if remaining_gids:
-        raise mb_exceptions.NoDataFoundException("Couldn't find an entity with id(s): {mbids}".format(mbids=remaining_gids))
+        raise mb_exceptions.NoDataFoundException("Couldn't find entities with IDs: {mbids}".format(mbids=remaining_gids))
     return entities
