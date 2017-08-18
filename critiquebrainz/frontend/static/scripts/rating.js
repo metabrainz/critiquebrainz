6b29fea439a18e8e10f9acfe6de82265e2672f9f
Pinank Solanki <solankipinank10@gmail.com>
2017-08-18 11:48:11 +0530

    Change styling of rating icons to main.less from rating.js

--- critiquebrainz/frontend/static/scripts/rating.js
+++ critiquebrainz/frontend/static/scripts/rating.js
@@ -2,16 +2,3 @@ require('bootstrap-rating-input');
 
 $.fn.rating.Constructor.DEFAULTS.clearable = '';
 $.fn.rating.Constructor.DEFAULTS.clearableIcon = 'glyphicon-remove-circle';
-
-$(document).ready(function() {
-  // Set color and size of stars icon
-  $('.glyphicon-star, .glyphicon-star-empty').css({'color': '#DAA520', 'font-size': '18px'});
-  // Set color and size of delete icon
-  $('.glyphicon-remove-circle').css({'color': '#808080', 'font-size': '16px'});
-  $('.glyphicon-remove-circle').hover(function() {
-    $(this).css('color', 'red');
-    }, function() {
-    $(this).css('color', '#808080');
-    });
-  $('.glyphicon-remove-circle').attr('title', 'Delete');
-});
