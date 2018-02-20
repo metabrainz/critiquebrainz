7f4d71aa559c1dc6a20a808ee62402bbaa0ab367
Suyash Garg <suyashgargsfam@gmail.com>
2018-02-20 06:04:00 +0530

    CB-280: Fix Spotify mapping page suggestions
    Fix missing suggestions after caching and add test for cached spotify
    results

--- critiquebrainz/frontend/external/spotify_test.py
+++ critiquebrainz/frontend/external/spotify_test.py
@@ -1,3 +1,4 @@
+from brainzutils import cache
 from critiquebrainz.frontend.testing import FrontendTestCase
 from critiquebrainz.frontend.external import spotify
 
@@ -8,6 +9,13 @@ class FakeSpotifyResponse:
 
     def json(self):
         return dict(url=self.url)
+
+    @classmethod
+    def fromSpotifyIds(cls, spotify_ids):
+        response = []
+        for spotify_id in spotify_ids:
+            response.append({'id': spotify_id, 'data': spotify._BASE_URL + spotify_id})
+        return dict(albums=response)
 
 
 class SpotifyTestCase(FrontendTestCase):
@@ -26,3 +34,25 @@ class SpotifyTestCase(FrontendTestCase):
         self.assertDictEqual(
             spotify.get_album('random-spotify-id'),
             dict(url="https://api.spotify.com/v1/albums/random-spotify-id"))
+
+    def test_get_multiple_albums(self):
+        spotify_ids = ['0Y7qkJVZ06tS2GUCDptzyW']
+        if cache.get_many(spotify_ids, 'spotify_albums'):
+            cache.delete_many(spotify_ids, 'spotify_albums')
+
+        spotify._get = lambda query: FakeSpotifyResponse.fromSpotifyIds(spotify_ids)
+        albums = spotify.get_multiple_albums(spotify_ids)
+        self.assertDictEqual(albums[spotify_ids[0]], {
+            'id': spotify_ids[0],
+            'data': spotify._BASE_URL + spotify_ids[0],
+        })
+
+        albums = cache.get_many(spotify_ids, 'spotify_albums')
+        self.assertListEqual(list(albums.keys()), spotify_ids)
+
+        # test if cached result is returned properly
+        albums = spotify.get_multiple_albums(spotify_ids)
+        self.assertDictEqual(albums[spotify_ids[0]], {
+            'id': spotify_ids[0],
+            'data': spotify._BASE_URL + spotify_ids[0],
+        })
