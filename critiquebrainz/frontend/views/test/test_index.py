437096f109eae326c5ae34cc4b7d1ff6d39575d4
Param Singh <param@metabrainz.org>
2018-02-23 00:41:58 +0530

    Initialize Flask-DebugToolbar correctly
    Also, add a test to ensure that it works

--- critiquebrainz/frontend/views/test/test_index.py
+++ critiquebrainz/frontend/views/test/test_index.py
@@ -1,3 +1,4 @@
+from critiquebrainz.frontend import create_app
 from critiquebrainz.frontend.testing import FrontendTestCase
 
 
@@ -14,3 +15,15 @@ class ViewsTestCase(FrontendTestCase):
     def test_guidelines(self):
         response = self.client.get("/guidelines")
         self.assert200(response)
+
+    def test_flask_debugtoolbar(self):
+        """ Test if flask debugtoolbar is loaded correctly
+
+        Creating an app with default config so that debug is True
+        and SECRET_KEY is defined.
+        """
+        app = create_app(debug=True)
+        client = app.test_client()
+        resp = client.get("/about")
+        self.assert200(resp)
+        self.assertIn("flDebug", str(resp.data))
