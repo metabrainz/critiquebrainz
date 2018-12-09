8b9ac4364bea0e3f1830f2810763ed60e947e5be
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-12-09 16:16:33 +0530

    Added view for rating from entity page along with tests

--- critiquebrainz/frontend/views/rate.py
+++ critiquebrainz/frontend/views/rate.py
@@ -1,3 +1,21 @@
+# critiquebrainz - Repository for Creative Commons licensed reviews
+#
+# Copyright (C) 2018 MetaBrainz Foundation Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from flask import Blueprint, redirect, url_for
 from flask_login import current_user, login_required
 from flask_babel import gettext
