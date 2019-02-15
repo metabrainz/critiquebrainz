7786499e39791e69f8e9959932bd8eb08f16448c
code-master5 <lauhny.bimalk@gmail.com>
2019-02-15 21:54:56 +0530

    Add statistics view and template

--- critiquebrainz/frontend/__init__.py
+++ critiquebrainz/frontend/__init__.py
@@ -146,6 +146,7 @@ def create_app(debug=None, config_path=None):
     from critiquebrainz.frontend.views.log import log_bp
     from critiquebrainz.frontend.views.comment import comment_bp
     from critiquebrainz.frontend.views.rate import rate_bp
+    from critiquebrainz.frontend.views.statistics import statistics_bp
 
     app.register_blueprint(frontend_bp)
     app.register_blueprint(review_bp, url_prefix='/review')
@@ -166,6 +167,7 @@ def create_app(debug=None, config_path=None):
     app.register_blueprint(moderators_bp, url_prefix='/moderators')
     app.register_blueprint(comment_bp, url_prefix='/comments')
     app.register_blueprint(rate_bp, url_prefix='/rate')
+    app.register_blueprint(statistics_bp, url_prefix='/statistics')
 
     return app
 
