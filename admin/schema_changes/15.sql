74cee902595fbbb11b387417e8b693f3e85bb7e0
Param Singh <param@metabrainz.org>
2019-02-18 13:44:16 +0530

    Remove the transaction from the schema change script

--- admin/schema_changes/15.sql
+++ admin/schema_changes/15.sql
@@ -1,6 +1,5 @@
-BEGIN;
-
+-- NOTE: this cannot be inside a transaction because
+-- postgres currently does not support adding values
+-- to enums inside a transaction block
 ALTER TYPE action_types ADD VALUE 'unhide_review' AFTER 'hide_review';
 ALTER TYPE action_types ADD VALUE 'unblock_user' AFTER 'block_user';
-
-COMMIT;
