03b8975e76f58e4be4dad22d4accd1b4b42a220f
code-master5 <lauhny.bimalk@gmail.com>
2019-04-08 23:13:39 +0530

    Add support for dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,61 +5,65 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 
 const frontEndStaticDir = path.resolve(__dirname, 'critiquebrainz/frontend/static');
 
-module.exports = {
-  entry: {
-    'main': path.resolve(frontEndStaticDir, 'scripts/main.js'),
-    'common': path.resolve(frontEndStaticDir, 'scripts/common.js'),
-    'leaflet': path.resolve(frontEndStaticDir, 'scripts/leaflet.js'),
-    'rating': path.resolve(frontEndStaticDir, 'scripts/rating.js'),
-    'spotify': path.resolve(frontEndStaticDir, 'scripts/spotify.js'),
-    'wysiwyg-editor': path.resolve(frontEndStaticDir, 'scripts/wysiwyg-editor.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'critiquebrainz/frontend/static/build/'),
-    filename: '[name].[contenthash].js',
-  },
-  module: {
-    rules: [
-    {
-      test: /\.(js)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: 'babel-loader'
-        }
+module.exports = (env, argv) => {
+  const isProd = argv.mode == 'production';
+  return {
+    mode: argv.mode,
+    entry: {
+      'main': path.resolve(frontEndStaticDir, 'scripts/main.js'),
+      'common': path.resolve(frontEndStaticDir, 'scripts/common.js'),
+      'leaflet': path.resolve(frontEndStaticDir, 'scripts/leaflet.js'),
+      'rating': path.resolve(frontEndStaticDir, 'scripts/rating.js'),
+      'spotify': path.resolve(frontEndStaticDir, 'scripts/spotify.js'),
+      'wysiwyg-editor': path.resolve(frontEndStaticDir, 'scripts/wysiwyg-editor.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, 'critiquebrainz/frontend/static/build/'),
+      filename: '[name].[contenthash].js',
+    },
+    module: {
+      rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              relativeUrls: false
+            }
+          }
+        ]
+      }
       ]
     },
-    {
-      test: /\.less$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: MiniCssExtractPlugin.loader
-        }, 
-        {
-          loader: "css-loader",
-          options: {
-            url: false 
-          }
-        },
-        {
-          loader: "less-loader",
-          options: {
-            relativeUrls: false
-          }
-        }
-      ]
-    }
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].css'
+      }),
+      new CleanWebpackPlugin(),
+      new ManifestPlugin()
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[name].[contenthash].css'
-    }),
-    new CleanWebpackPlugin(),
-    new ManifestPlugin()
-  ]
+  }
 };
