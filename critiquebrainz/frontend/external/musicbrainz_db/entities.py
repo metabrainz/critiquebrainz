2cd202d38329da37b079832e5aa6fe130ceffc32
spellew <spellew1298@gmail.com>
2019-06-16 12:22:42 -0400

    Implemented the retrieval of labels through brainzutils

--- critiquebrainz/frontend/external/musicbrainz_db/entities.py
+++ critiquebrainz/frontend/external/musicbrainz_db/entities.py
@@ -1,12 +1,13 @@
 from brainzutils.musicbrainz_db.artist import fetch_multiple_artists
+from brainzutils.musicbrainz_db.label import fetch_multiple_labels
 from brainzutils.musicbrainz_db.place import fetch_multiple_places
 from brainzutils.musicbrainz_db.event import fetch_multiple_events
 from brainzutils.musicbrainz_db.release_group import fetch_multiple_release_groups
 from critiquebrainz.frontend.external.musicbrainz_db.release_group import get_release_group_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.place import get_place_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.event import get_event_by_id
+from critiquebrainz.frontend.external.musicbrainz_db.label import get_label_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.artist import get_artist_by_id
-
 
 def get_multiple_entities(entities):
     """Fetch multiple entities using their MBIDs.
@@ -26,6 +27,7 @@ def get_multiple_entities(entities):
     entities_info = {}
     release_group_mbids = [entity[0] for entity in filter(lambda entity: entity[1] == 'release_group', entities)]
     artist_mbids = [entity[0] for entity in filter(lambda entity: entity[1] == 'artist', entities)]
+    label_mbids = [entity[0] for entity in filter(lambda entity: entity[1] == 'label', entities)]
     place_mbids = [entity[0] for entity in filter(lambda entity: entity[1] == 'place', entities)]
     event_mbids = [entity[0] for entity in filter(lambda entity: entity[1] == 'event', entities)]
     entities_info.update(fetch_multiple_release_groups(
@@ -34,6 +36,9 @@ def get_multiple_entities(entities):
     ))
     entities_info.update(fetch_multiple_artists(
         artist_mbids,
+    ))
+    entities_info.update(fetch_multiple_labels(
+        label_mbids,
     ))
     entities_info.update(fetch_multiple_places(
         place_mbids,
@@ -50,6 +55,8 @@ def get_entity_by_id(id, type='release_group'):
         entity = get_release_group_by_id(str(id))
     elif type == 'artist':
         entity = get_artist_by_id(str(id))
+    elif type == 'label':
+        entity = get_label_by_id(str(id))
     elif type == 'place':
         entity = get_place_by_id(str(id))
     elif type == 'event':
