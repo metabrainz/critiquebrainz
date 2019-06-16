e4d1bc4d19a8d143b3eb86322fe479ba7117b5d4
spellew <spellew1298@gmail.com>
2019-06-16 13:35:11 -0400

    Implemented the searching of recordings

--- critiquebrainz/frontend/external/musicbrainz.py
+++ critiquebrainz/frontend/external/musicbrainz.py
@@ -44,3 +44,9 @@ def search_places(query='', limit=None, offset=None):
     """Search for places."""
     api_resp = musicbrainzngs.search_places(query=query, limit=limit, offset=offset)
     return api_resp.get('place-count'), api_resp.get('place-list')
+
+
+def search_recordings(query='', limit=None, offset=None):
+    """Search for recordings."""
+    api_resp = musicbrainzngs.search_recordings(query=query, limit=limit, offset=offset)
+    return api_resp.get('recording-count'), api_resp.get('recording-list')
