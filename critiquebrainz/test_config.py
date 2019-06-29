982378e2c6775669bab65388c3d742987a4b14b4
spellew <spellew1298@gmail.com>
2019-06-28 20:54:39 -0400

    Updated docker-compose.test.yml and MB_DATABASE_URI

--- critiquebrainz/test_config.py
+++ critiquebrainz/test_config.py
@@ -4,7 +4,7 @@ SECRET_KEY = "test"
 WTF_CSRF_ENABLED = False
 
 SQLALCHEMY_DATABASE_URI = "postgresql://critiquebrainz:critiquebrainz@db_test:5432/critiquebrainz"
-MB_DATABASE_URI = "postgresql://musicbrainz:musicbrainz@musicbrainz_db:5432/musicbrainz_test"
+MB_DATABASE_URI = "postgresql://musicbrainz:musicbrainz@musicbrainz_db:5432/musicbrainz_db"
 
 # Logging
 LOG_FILE = None
