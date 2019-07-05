3a8ae176e7a1b84c614291b6cb2e0d9d08f77eb2
spellew <spellew1298@gmail.com>
2019-07-04 20:48:49 -0400

    Fixed tests

--- critiquebrainz/frontend/external/musicbrainz_db/entities.py
+++ critiquebrainz/frontend/external/musicbrainz_db/entities.py
@@ -1,8 +1,8 @@
+from brainzutils.musicbrainz_db.artist import fetch_multiple_artists
 from critiquebrainz.frontend.external.musicbrainz_db.release_group import fetch_multiple_release_groups, get_release_group_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.place import fetch_multiple_places, get_place_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.event import fetch_multiple_events, get_event_by_id
 from critiquebrainz.frontend.external.musicbrainz_db.artist import get_artist_by_id
-from brainzutils.musicbrainz_db.artist import fetch_multiple_artists
 
 
 def get_multiple_entities(entities):
