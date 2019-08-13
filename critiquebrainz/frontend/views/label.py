f134e12b40d0213379c505ca3ec388d7e4e25a76
spellew <spellew1298@gmail.com>
2019-08-12 20:23:01 -0400

    Added rating support for labels

--- critiquebrainz/frontend/views/label.py
+++ critiquebrainz/frontend/views/label.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template, request
 from werkzeug.exceptions import NotFound
+from flask_login import current_user
 from flask_babel import gettext
 import critiquebrainz.db.review as db_review
 import critiquebrainz.frontend.external.musicbrainz_db.label as mb_label
 import critiquebrainz.frontend.external.musicbrainz_db.exceptions as mb_exceptions
-from critiquebrainz.frontend.views import LABEL_REVIEWS_LIMIT
+from critiquebrainz.frontend.views import get_avg_rating, LABEL_REVIEWS_LIMIT
+from critiquebrainz.frontend.forms.rate import RatingEditForm
+
 
 label_bp = Blueprint('label', __name__)
 
@@ -21,6 +24,16 @@ def entity(id):
     if request.args.get('reviews') == "all":
         label_reviews_limit = None
 
+    if current_user.is_authenticated:
+        my_reviews, my_count = db_review.list_reviews(
+            entity_id=label['id'],
+            entity_type='label',
+            user_id=current_user.id,
+        )
+        my_review = my_reviews[0] if my_count else None
+    else:
+        my_review = None
+
     reviews_offset = 0
     reviews, reviews_count = db_review.list_reviews(
         entity_id=label['id'],
@@ -30,6 +43,11 @@ def entity(id):
         offset=reviews_offset,
     )
 
+    avg_rating = get_avg_rating(label['id'], "label")
+
+    rating_form = RatingEditForm(entity_id=id, entity_type='label')
+    rating_form.rating.data = my_review['rating'] if my_review else None
+
     return render_template(
         'label/entity.html',
         id=label['id'],
@@ -37,4 +55,6 @@ def entity(id):
         reviews=reviews,
         reviews_limit=label_reviews_limit,
         reviews_count=reviews_count,
+        avg_rating=avg_rating,
+        rating_form=rating_form,
     )
