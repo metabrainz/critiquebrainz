755c8114b5c2fd292eea2358d82954a19649c58f
Kartik Ohri <kartikohri13@gmail.com>
2020-09-23 20:05:47 +0530

    Delete init_postgres method

--- manage.py
+++ manage.py
@@ -115,7 +115,6 @@ def init_db(skip_create_db=False, test_db=False, force=False):
         db_uri = frontend.create_app().config['SQLALCHEMY_DATABASE_URI']
 
     if not skip_create_db:
-        init_postgres(db_uri)
         create_extension(db_uri)
 
     click.echo("Creating tables... ", nl=False)
@@ -129,37 +128,6 @@ def init_db(skip_create_db=False, test_db=False, force=False):
     click.echo("Done!")
 
     click.echo("Initialization has been completed!")
-
-
-def init_postgres(db_uri):
-    """Initializes PostgreSQL database from provided URI.
-
-    New user and database will be created, if needed. It also creates uuid-ossp extension.
-    """
-    hostname, port, db, username, password = data_utils.explode_db_uri(db_uri)  # pylint: disable=unused-variable
-    if hostname not in ['localhost', '127.0.0.1']:
-        raise Exception('Cannot configure a remote database')
-
-    # Checking if user already exists
-    retv = subprocess.check_output('sudo -u postgres psql -t -A -c'
-                                   '"SELECT COUNT(*) FROM pg_user WHERE usename = \'%s\';"' %
-                                   username, shell=True)
-    if retv == '0':
-        exit_code = subprocess.call(
-            'sudo -u postgres psql -c '
-            '"CREATE ROLE %s PASSWORD \'%s\' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"' %
-            (username, password),
-            shell=True,
-        )
-        if exit_code != 0:
-            raise Exception('Failed to create PostgreSQL user!')
-
-    # Checking if database exists
-    exit_code = subprocess.call('sudo -u postgres psql -c "\\q" %s' % db, shell=True)
-    if exit_code != 0:
-        exit_code = subprocess.call('sudo -u postgres createdb -O %s %s' % (username, db), shell=True)
-        if exit_code != 0:
-            raise Exception('Failed to create PostgreSQL database!')
 
 
 def create_extension(db_uri):
