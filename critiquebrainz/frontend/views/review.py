5f2a8eb169f414f5c05017e8fc77401c7b359c2d
Abhinav Ohri <abhinavohri13@gmail.com>
2020-10-31 00:16:53 +0530

    Made the necessary changes from the code review

--- critiquebrainz/frontend/views/review.py
+++ critiquebrainz/frontend/views/review.py
@@ -289,7 +289,7 @@ def create(entity_type=None, entity_id=None):
     if not form.errors:
         flash.info(gettext("Please provide some text or a rating for this review."))
     return render_template('review/modify/write.html', form=form, entity_type=entity_type,
-                           entity_title=entity_title, entity=entity, entity_id=entity_id)
+                           entity_title=entity_title, entity=entity)
 
 
 @review_bp.route('/<uuid:id>/edit', methods=('GET', 'POST'))
