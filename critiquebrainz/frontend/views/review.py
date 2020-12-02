b2b30a048708d6a0170e6f5f380ff7ba440d75b7
abhinavohri <abhinavohri13@gmail.com>
2020-12-02 11:59:27 +0530

    Removed duplicate flash messages

--- critiquebrainz/frontend/views/review.py
+++ critiquebrainz/frontend/views/review.py
@@ -100,7 +100,6 @@ def entity(id, rev=None):
         if not current_user.is_admin():
             raise Forbidden(gettext("Review has been hidden. "
                                     "You need to be an administrator to view it."))
-        flash.warn(gettext("Review has been hidden."))
 
     spotify_mappings = None
     soundcloud_url = None
