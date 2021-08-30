68a4a9dfedc24d349c9ff37e4a282775b0ff9524
Kartik Ohri <kartikohri13@gmail.com>
2021-08-30 16:37:19 +0530

    Check if access_token is None before accessing it

--- critiquebrainz/ws/oauth/views.py
+++ critiquebrainz/ws/oauth/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 
 from critiquebrainz.decorators import nocache, crossdomain
 from critiquebrainz.ws.oauth import oauth
-from critiquebrainz.ws.oauth.exceptions import UnsupportedGrantType
+from critiquebrainz.ws.oauth.exceptions import UnsupportedGrantType, InvalidGrant
 
 oauth_bp = Blueprint('ws_oauth', __name__)
 
@@ -37,6 +37,8 @@ def oauth_token_handler():
         scope = grant['scopes']
     elif grant_type == 'refresh_token':
         token = oauth.fetch_token(client_id, refresh_token)
+        if token is None:
+            raise InvalidGrant()
         user_id = token['user_id']
         scope = token['scopes']
     else:
