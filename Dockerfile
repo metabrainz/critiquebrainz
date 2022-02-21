metabrainz/critiquebrainz

commit 4525f90024cb0bd7e11af093605f1add05e7691c

startup cleanups

Dockerfile
@@ -1,7 +1,5 @@
 FROM metabrainz/python:3.8-20210115
 
-ARG DEPLOY_ENV
-
 RUN apt-get update \
      && apt-get install -y --no-install-recommends \
                         build-essential \
@@ -58,27 +56,28 @@ RUN useradd --create-home --shell /bin/bash critiquebrainz
 # Services #
 ############
 
-COPY ./docker/consul-template-uswgi.conf /etc/consul-template-uswgi.conf
-COPY ./docker/consul-template-cron.conf /etc/consul-template-cron.conf
-
 # runit service files
 # All services are created with a `down` file, preventing them from starting
 # rc.local removes the down file for the specific service we want to run in a container
 # http://smarden.org/runit/runsv.8.html
 
-COPY ./docker/$DEPLOY_ENV/uwsgi/uwsgi.service /etc/service/uwsgi/run
-RUN chmod 755 /etc/service/uwsgi/run
-COPY ./docker/$DEPLOY_ENV/uwsgi/uwsgi.ini /etc/uwsgi/uwsgi.ini
-RUN touch /etc/service/uswgi/down
+COPY ./docker/rc.local /etc/rc.local
+
+# UWSGI
+COPY ./docker/uwsgi/consul-template-uwsgi.conf /etc/consul-template-uwsgi.conf
+COPY ./docker/uwsgi/uwsgi.service /etc/service/uwsgi/run
+COPY ./docker/uwsgi/uwsgi.ini /etc/uwsgi/uwsgi.ini
+RUN touch /etc/service/uwsgi/down
 
 # cron jobs
-COPY ./docker/$DEPLOY_ENV/cron/cron.service /etc/service/cron/run
-ADD ./docker/prod/cron/jobs /tmp/crontab
-RUN chmod 0644 /tmp/crontab && crontab -u critiquebrainz /tmp/crontab
-RUN rm /tmp/crontab
+COPY ./docker/cron/consul-template-cron-config.conf /etc/consul-template-cron-config.conf
+COPY ./docker/cron/cron-config.service /etc/service/cron-config/run
+COPY ./docker/cron/crontab /etc/cron.d/critiquebrainz
+RUN touch /etc/service/cron/down
+RUN touch /etc/service/cron-config/down
+
 RUN touch /var/log/dump_backup.log /var/log/public_dump_create.log /var/log/json_dump_create.log \
     && chown critiquebrainz:critiquebrainz /var/log/dump_backup.log /var/log/public_dump_create.log /var/log/json_dump_create.log
-RUN touch /etc/service/cron/down
 
 ARG GIT_COMMIT_SHA
 ENV GIT_SHA ${GIT_COMMIT_SHA}

docker/beta/uwsgi/uwsgi.service
@@ -1,3 +1,0 @@
-#!/bin/sh
-
-exec uwsgi /etc/uwsgi/uwsgi.ini

docker/cron/consul-template-cron-config.conf
@@ -2,6 +2,7 @@ template {
     source = "/code/consul_config.py.ctmpl"
     destination = "/code/consul_config.py"
 }
+
 template {
     source = "/code/admin/config.sh.ctmpl"
     destination = "/code/admin/config.sh"

docker/cron/cron-config.service
@@ -1,0 +1,3 @@
+#!/bin/bash
+
+exec run-consul-template -config /etc/consul-template-cron-config.conf

docker/cron/crontab
@@ -1,0 +1,13 @@
+PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+
+# Database backup creation
+10 00 * * * critiquebrainz /usr/local/bin/python /code/manage.py dump full_db -l /data/backups -r >> /var/log/dump_backup.log 2>&1
+
+# Public MB-style dump creation
+15 00 * * * critiquebrainz /usr/local/bin/python /code/manage.py dump public -l /data/dumps/dump -r >> /var/log/public_dump_create.log 2>&1
+
+# JSON dump creation
+20 00 * * * critiquebrainz /usr/local/bin/python /code/manage.py dump json -l /data/dumps/json -r >> /var/log/json_dump_create.log 2>&1
+
+# Copy everything over...
+45 00 * * * critiquebrainz /code/admin/rsync-dump-files.sh

docker/prod/cron/cron.service
@@ -1,3 +1,0 @@
-#!/bin/bash
-
-exec run-consul-template -config /etc/consul-template-cron.conf

docker/prod/cron/jobs
@@ -1,14 +1,0 @@
-PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
-PYTHONPATH=/usr/local/lib/python36.zip:/usr/local/lib/python3.6:/usr/local/lib/python3.6/lib-dynload:/usr/local/lib/python3.6/site-packages:/usr/local/lib/python3.6/site-packages/messybrainz-99.0.0.dev0-py3.6.egg
-
-# Database backup creation
-10 00 * * * /usr/local/bin/python /code/manage.py dump full_db -l /data/backups -r >> /var/log/dump_backup.log 2>&1
-
-# Public MB-style dump creation
-15 00 * * * /usr/local/bin/python /code/manage.py dump public -l /data/dumps/dump -r >> /var/log/public_dump_create.log 2>&1
-
-# JSON dump creation
-20 00 * * * /usr/local/bin/python /code/manage.py dump json -l /data/dumps/json -r >> /var/log/json_dump_create.log 2>&1
-
-# Copy everything over...
-45 00 * * * /code/admin/rsync-dump-files.sh

docker/prod/uwsgi/uwsgi.ini
@@ -1,8 +1,0 @@
-[uwsgi]
-master = true
-socket = 0.0.0.0:13032
-module = manage
-callable = application
-chdir = /code/
-enable-threads = true
-processes = 20

commit 34446351b0dfb7c035a6e6fb38f864770427bbfd

Fix crontab permissions
Broken since 4525f9002

Dockerfile
@@ -83,6 +83,7 @@ RUN touch /etc/service/uwsgi/down
 COPY ./docker/cron/consul-template-cron-config.conf /etc/consul-template-cron-config.conf
 COPY ./docker/cron/cron-config.service /etc/service/cron-config/run
 COPY ./docker/cron/crontab /etc/cron.d/critiquebrainz
+RUN chmod 0644 /etc/cron.d/critiquebrainz
 RUN touch /etc/service/cron/down
 RUN touch /etc/service/cron-config/down
 