bff6b412b778727f998d16fce76e89c13ef18c05
Alastair Porter <alastair@metabrainz.org>
2022-03-29 15:15:37 +0200

    Use test config file in test request context
    Should have been reverted in 754f5a71c6 but was missed

--- critiquebrainz/data/utils.py
+++ critiquebrainz/data/utils.py
@@ -112,7 +112,9 @@ def with_test_request_context(f):
 
     @wraps(f)
     def decorated(*args, **kwargs):
-        with frontend.create_app().test_request_context():
+        with frontend.create_app(config_path=os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    '..', '..', 'test_config.py')).test_request_context():
             return f(*args, **kwargs)
 
     return decorated
