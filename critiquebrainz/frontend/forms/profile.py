bda24202c40f6b0f759b8918f2679138a522d825
Kartik Ohri <kartikohri13@gmail.com>
2022-04-05 14:00:12 +0530

    Try WTForms == 2.3.3 instead

--- critiquebrainz/frontend/forms/profile.py
+++ critiquebrainz/frontend/forms/profile.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_babel import lazy_gettext
 from wtforms import StringField, BooleanField, RadioField, validators
-from wtforms.fields import EmailField
+from wtforms.fields.html5 import EmailField
 
 
 class ProfileEditForm(FlaskForm):
