e2e5363537f624c9ea7b8b0cce6a30135b0b94e1
Ansh Goyal <anshg1214@gmail.com>
2022-04-21 11:47:52 +0000

    Add function to fetch events for place from BU

--- critiquebrainz/frontend/external/musicbrainz_db/event.py
+++ critiquebrainz/frontend/external/musicbrainz_db/event.py
@@ -23,3 +23,31 @@ def get_event_by_mbid(mbid):
             return None
         cache.set(key, event, DEFAULT_CACHE_EXPIRATION)
     return event
+
+
+def get_event_for_place(place_id, event_types=None, limit=None, offset=None):
+    """Get events for the place.
+
+    Args:
+        place_id (uuid): MBID(gid) of the place.
+        event_types (list): List of event types to be fetched.
+        limit (int): Max number of events to return.
+        offset (int): Offset that can be used in conjunction with the limit.
+
+    Returns:
+        Tuple containing the list of dictionaries of events ordered by begin year
+        and the total count of the events.
+    """
+    key = cache.gen_key( place_id, limit, offset, *event_types)
+    events = cache.get(key)
+    if not events:
+        events = db.get_event_for_place(
+            place_id,
+            event_types=event_types,
+            limit=limit,
+            offset=offset,
+        )
+        if not events:
+            return None
+        cache.set(key, events, DEFAULT_CACHE_EXPIRATION)
+    return events
