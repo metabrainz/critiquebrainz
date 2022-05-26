cba42772fbe34ee101b7ea47b81771c0bb489945
Ansh Goyal <anshg1214@gmail.com>
2022-05-26 18:07:30 +0000

    Add bookbrainz database

---
+++ docker/pg_custom/create-bb-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER critiquebrainz WITH SUPERUSER PASSWORD 'critiquebrainz';
+    CREATE DATABASE critiquebrainz;
+    GRANT ALL PRIVILEGES ON DATABASE critiquebrainz TO critiquebrainz;
+    CREATE USER bookbrainz WITH SUPERUSER PASSWORD 'bookbrainz';
+    CREATE DATABASE bookbrainz;
+    GRANT ALL PRIVILEGES ON DATABASE bookbrainz TO bookbrainz;
+EOSQL
