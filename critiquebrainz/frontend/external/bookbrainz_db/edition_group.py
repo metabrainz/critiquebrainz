9da362afb7ae57af94d5f69e341afb03be41066a
Ansh Goyal <anshg1214@gmail.com>
2022-06-03 12:30:55 +0000

    Fetch Edition Group

---
+++ critiquebrainz/frontend/external/bookbrainz_db/edition_group.py
@@ -0,0 +1,76 @@
+import uuid
+from brainzutils import cache
+from typing import List
+import sqlalchemy
+import critiquebrainz.frontend.external.bookbrainz_db as db 
+from critiquebrainz.frontend.external.bookbrainz_db import DEFAULT_CACHE_EXPIRATION
+from critiquebrainz.frontend.external.bookbrainz_db.identifiers import fetch_identifiers
+
+
+def get_edition_group_by_bbid(bbid: uuid.UUID):
+    """
+    Get info related to an edition group using its BookBrainz ID.
+    Args:
+        bbid : BBID of the edition group.
+    Returns:
+        A dictionary containing the basic information related to the edition group.
+    """
+    edition_group = fetch_multiple_edition_groups([bbid])
+    if not edition_group:
+        return None
+    return edition_group[bbid]
+
+
+def fetch_multiple_edition_groups(bbids: List[uuid.UUID]) -> dict:
+    """
+    Get info related to multiple edition groups using their BookBrainz IDs. 
+    Args:
+        bbids (list): List of BBID of edition groups.
+    Returns:
+        A dictionary containing info of multiple edition groups keyed by their BBID.
+    """
+    key = cache.gen_key('edition-groups', bbids)
+    edition_groups = cache.get(key)
+    if not edition_groups:
+        with db.bb_engine.connect() as connection:
+            result = connection.execute(sqlalchemy.text("""
+                SELECT 
+                    e.bbid as bbid,
+                    al.name as name,
+                    al.sort_name as sort_name,
+                    al.primary as primary,
+                    egd.identifier_set_id as identifier_set_id,
+                    egd.relationship_set_id as relationship_set_id,
+                    egd.author_credit_id as author_credit_id,
+                    disam.comment as disambiguation_comment,
+                    egtype.label as label,
+                    acn.author_bbid as author_bbid,
+                    acn.name as author_name
+                
+                FROM bookbrainz.entity e
+                LEFT JOIN bookbrainz.edition_group_header c ON c.bbid = e.bbid
+                LEFT JOIN bookbrainz.edition_group_revision cr on c.master_revision_id = cr.id
+                LEFT JOIN bookbrainz.edition_group_data egd ON cr.data_id = egd.id
+                LEFT JOIN bookbrainz.alias_set als ON egd.alias_set_id = als.id
+                LEFT JOIN bookbrainz.alias al ON als.default_alias_id = al.id
+                LEFT JOIN bookbrainz.edition_group_type egtype ON egd.type_id = egtype.id
+                LEFT JOIN bookbrainz.disambiguation disam on egd.disambiguation_id = disam.id
+                LEFT JOIN bookbrainz.author_credit_name acn on egd.author_credit_id = acn.author_credit_id
+                WHERE e.bbid in :bbids;
+                
+                """), {'bbids': tuple(bbids)})
+            
+            edition_groups = result.fetchall()
+            results = {}
+            for edition_group in edition_groups:
+                edition_group = dict(edition_group)
+                edition_group['bbid'] = str(edition_group['bbid'])
+                edition_group['identifiers'] = fetch_identifiers(edition_group['identifier_set_id'])
+                results[edition_group['bbid']] = edition_group
+            
+            edition_groups = results
+            cache.set(key, edition_groups, DEFAULT_CACHE_EXPIRATION)
+    
+    if not edition_groups:
+        return None
+    return edition_groups
---
+++ critiquebrainz/frontend/external/bookbrainz_db/identifiers.py
@@ -0,0 +1,92 @@
+from typing import List
+from brainzutils import cache
+import sqlalchemy
+import critiquebrainz.frontend.external.bookbrainz_db as db
+from critiquebrainz.frontend.external.bookbrainz_db import DEFAULT_CACHE_EXPIRATION
+
+def fetch_identifiers(identifier_set_id: int) -> List:
+    """
+    Fetch identifiers from the database.
+    Args:
+        identifier_set_id (int): Identifier set ID.
+    Returns:
+        List of identifiers.
+    """
+    key = cache.gen_key('identifier', identifier_set_id)
+    identifiers = cache.get(key)
+    if not identifiers:
+        with db.bb_engine.connect() as connection:
+            result = connection.execute(sqlalchemy.text("""
+                SELECT
+                    iden.type_id as type_id,
+                    idtype.label as label,
+                    iden.value as value
+                FROM bookbrainz.identifier_set__identifier idens
+                LEFT JOIN bookbrainz.identifier iden on idens.identifier_id = iden.id
+                LEFT JOIN bookbrainz.identifier_type idtype on iden.type_id = idtype.id
+                WHERE idens.set_id = :identifier_set_id;
+                """), ({'identifier_set_id': identifier_set_id}))
+            identifiers = result.fetchall()
+            identifiers = [dict(identifier) for identifier in identifiers]
+            identifiers = process_identifiers(identifiers)
+            cache.set(key, identifiers, DEFAULT_CACHE_EXPIRATION)
+
+    if not identifiers:
+        return None
+    return identifiers
+
+
+def process_identifiers(identifiers: List) -> List:
+    """Process identifiers and include urls."""
+    external_urls = []
+
+    url_map = {
+        1: "https://musicbrainz.org/release/",
+        2: "https://musicbrainz.org/artist/",
+        3: "https://musicbrainz.org/work/",
+        4: "https://www.wikidata.org/wiki/",
+        5: "https://www.amazon.com/dp/",
+        6: "https://openlibrary.org/books/",
+        8: "https://openlibrary.org/works/",
+        9: "https://isbnsearch.org/isbn/",
+        10: "https://isbnsearch.org/isbn/",
+        11: "https://www.barcodelookup.com/",
+        12: "https://viaf.org/viaf/",
+        29: "https://viaf.org/viaf/",
+        31: "https://viaf.org/viaf/",
+        13: "http://www.isni.org/",
+        14: "https://www.librarything.com/work/",
+        15: "https://www.librarything.com/author/",
+        16: "https://www.imdb.com/title/",
+        17: "https://musicbrainz.org/label/",
+        18: "https://www.wikidata.org/wiki/",
+        19: "https://www.wikidata.org/wiki/",
+        20: "https://www.wikidata.org/wiki/",
+        21: "https://www.wikidata.org/wiki/",
+        30: "https://www.wikidata.org/wiki/",
+        22: "https://www.archive.org/details/",
+        23: "https://www.openlibrary.org/authors/",
+        24: "https://lccn.loc.gov/",
+        25: "https://www.orcid.org/",
+        26: "https://www.worldcat.org/oclc/",
+        27: "https://www.goodreads.com/author/show/",
+        28: "https://www.goodreads.com/book/show/",
+        32: "https://musicbrainz.org/series/",
+        33: "https://www.goodreads.com/series/",
+        34: "https://www.imdb.com/list/",
+    }
+    
+    for identifier in identifiers:
+        value = identifier['value']
+        type_id = identifier['type_id']
+        if type_id == 13: 
+            url = url_map[13] + value.replace(" ", "") # Remove spaces first (see BB-499)
+        else:
+            url = url_map[type_id] + value
+        external_urls.append({
+            'name': identifier['label'],
+            'url': url,
+            'value': value,
+        })
+
+    return external_urls
