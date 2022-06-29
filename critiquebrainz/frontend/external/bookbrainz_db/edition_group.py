c589b3bd640bdd627e57a6c0fcb1443f68a847a3
Ansh Goyal <anshg1214@gmail.com>
2022-06-29 10:40:27 +0000

    Used updated views to fetch edition_group info

--- critiquebrainz/frontend/external/bookbrainz_db/edition_group.py
+++ critiquebrainz/frontend/external/bookbrainz_db/edition_group.py
@@ -6,6 +6,7 @@ import critiquebrainz.frontend.external.bookbrainz_db as db
 from critiquebrainz.frontend.external.bookbrainz_db import DEFAULT_CACHE_EXPIRATION
 from critiquebrainz.frontend.external.bookbrainz_db.identifiers import fetch_identifiers
 from critiquebrainz.frontend.external.bookbrainz_db.relationships import fetch_relationships
+from critiquebrainz.frontend.external.bookbrainz_db.author_credits import fetch_author_credits
 
 
 def get_edition_group_by_bbid(bbid: uuid.UUID) -> dict:
@@ -38,30 +39,7 @@ def fetch_multiple_edition_groups(bbids: List[uuid.UUID]) -> dict:
     if not edition_groups:
         with db.bb_engine.connect() as connection:
             result = connection.execute(sqlalchemy.text("""
-                SELECT 
-                    e.bbid as bbid,
-                    al.name as name,
-                    al.sort_name as sort_name,
-                    al.primary as primary,
-                    egd.identifier_set_id as identifier_set_id,
-                    egd.relationship_set_id as relationship_set_id,
-                    egd.author_credit_id as author_credit_id,
-                    disam.comment as disambiguation_comment,
-                    egtype.label as label,
-                    acn.author_bbid as author_bbid,
-                    acn.name as author_name
-                
-                FROM bookbrainz.entity e
-                LEFT JOIN bookbrainz.edition_group_header c ON c.bbid = e.bbid
-                LEFT JOIN bookbrainz.edition_group_revision cr on c.master_revision_id = cr.id
-                LEFT JOIN bookbrainz.edition_group_data egd ON cr.data_id = egd.id
-                LEFT JOIN bookbrainz.alias_set als ON egd.alias_set_id = als.id
-                LEFT JOIN bookbrainz.alias al ON als.default_alias_id = al.id
-                LEFT JOIN bookbrainz.edition_group_type egtype ON egd.type_id = egtype.id
-                LEFT JOIN bookbrainz.disambiguation disam on egd.disambiguation_id = disam.id
-                LEFT JOIN bookbrainz.author_credit_name acn on egd.author_credit_id = acn.author_credit_id
-                WHERE e.bbid in :bbids;
-                
+                SELECT * FROM edition_group WHERE bbid in :bbids and master = 't';
                 """), {'bbids': tuple(bbids)})
             
             edition_groups = result.fetchall()
@@ -71,6 +49,7 @@ def fetch_multiple_edition_groups(bbids: List[uuid.UUID]) -> dict:
                 edition_group['bbid'] = str(edition_group['bbid'])
                 edition_group['identifiers'] = fetch_identifiers(edition_group['identifier_set_id'])
                 edition_group['rels'] = fetch_relationships( edition_group['relationship_set_id'], ['Edition'])
+                edition_group['author_credits'] = fetch_author_credits(edition_group['author_credit_id'])
                 results[edition_group['bbid']] = edition_group
             
             edition_groups = results
