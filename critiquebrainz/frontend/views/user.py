d6f670887f6b2f2f2255cb4fa84abbbf9cd03560
Ansh Goyal <anshg1214@gmail.com>
2022-08-22 09:34:13 +0000

    feat: Unify mappings for entity names

--- critiquebrainz/frontend/views/user.py
+++ critiquebrainz/frontend/views/user.py
@@ -49,20 +49,8 @@ def reviews(user_id):
     # happens so infrequently that we don't bother to back-fill it.
     retrieved_entity_mbids = entities_info.keys()
     reviews = [r for r in reviews if str(r["entity_id"]) in retrieved_entity_mbids]
-    
-    entity_names = {
-        'artist': gettext('Artist'),
-        'release_group': gettext('Release Group'),
-        'label': gettext('Label'),
-        'recording': gettext('Recording'),
-        'place': gettext('Place'),
-        'event': gettext('Event'),
-        'work': gettext('Work'),
-        'bb_edition_group': gettext('Edition Group'),
-        'bb_literary_work': gettext('Literary Work'),
-        'bb_author': gettext('Author'),
-        'bb_series': gettext('Series'),
-    }
+
+    entity_names = db_review.ENTITY_TYPES_MAPPING
 
     
     return render_template('user/reviews.html', section='reviews', user=user,
