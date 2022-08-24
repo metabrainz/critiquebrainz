73afc1cfa4e2df195f415e74c59e5e863764e96a
Ansh Goyal <anshg1214@gmail.com>
2022-08-24 19:26:05 +0000

    feat: Show Work-Literary Work Relationship

--- critiquebrainz/frontend/views/work.py
+++ critiquebrainz/frontend/views/work.py
@@ -7,6 +7,8 @@ import critiquebrainz.db.review as db_review
 import critiquebrainz.frontend.external.musicbrainz_db.work as mb_work
 from critiquebrainz.frontend.forms.rate import RatingEditForm
 from critiquebrainz.frontend.views import get_avg_rating, WORK_REVIEWS_LIMIT, BROWSE_RECORDING_LIMIT
+from critiquebrainz.frontend.external.bookbrainz_db.common_entity import get_literary_work_for_work
+from critiquebrainz.frontend.external.bookbrainz_db import literary_work as bb_literary_work
 
 work_bp = Blueprint('work', __name__)
 
@@ -17,6 +19,23 @@ def entity(id):
     work = mb_work.get_work_by_mbid(id)
     if work is None:
         raise NotFound(gettext("Sorry, we couldn't find a work with that MusicBrainz ID."))
+
+    literary_work_bbids = get_literary_work_for_work(id)
+    literary_work_info = {}
+    if literary_work_bbids:
+        literary_work_info = bb_literary_work.fetch_multiple_literary_works(literary_work_bbids)
+        for literary_work in literary_work_info:
+            reviews, count = db_review.list_reviews(
+                entity_id=literary_work,
+                entity_type='bb_literary_work',
+                sort='popularity',
+                limit=WORK_REVIEWS_LIMIT,
+                offset=0,
+            )
+            literary_work_info[literary_work]['reviews'] = reviews
+            literary_work_info[literary_work]['reviews_count'] = count
+
+    literary_work_info = literary_work_info.values()
 
     work_reviews_limit = WORK_REVIEWS_LIMIT
     if request.args.get('reviews') == "all":
@@ -49,7 +68,11 @@ def entity(id):
     if page < 1:
         return redirect(url_for('work.entity', id=id))
 
-    recording_rels = work['recording-rels']
+    if 'recording-rels' in work:
+        recording_rels = work['recording-rels']
+    else:
+        recording_rels = []
+
     recording_count = len(recording_rels)
 
     recording_offset = (page - 1) * BROWSE_RECORDING_LIMIT
@@ -60,8 +83,8 @@ def entity(id):
     rating_form = RatingEditForm(entity_id=id, entity_type='work')
     rating_form.rating.data = my_review['rating'] if my_review else None
 
-    return render_template('work/entity.html', id=work['mbid'], work=work, page=page,
-                           reviews=reviews, my_review=my_review, reviews_limit=work_reviews_limit,
+    return render_template('work/entity.html', id=work['mbid'], work=work, page=page, reviews=reviews,
+                           literary_work_info=literary_work_info, my_review=my_review, reviews_limit=work_reviews_limit,
                            reviews_count=reviews_count, avg_rating=avg_rating, rating_form=rating_form,
                            recording_limit=BROWSE_RECORDING_LIMIT, recording_rels=recording_rels,
                            recording_count=recording_count, current_user=current_user)
