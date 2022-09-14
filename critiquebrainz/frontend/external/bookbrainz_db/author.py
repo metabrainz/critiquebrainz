1d19285f81dec3d9db220b3954841b9cd8be735e
Ansh Goyal <anshg1214@gmail.com>
2022-09-14 13:50:40 +0000

    feat: Fetch Relationships and Identifiers along with entity in a single query

--- critiquebrainz/frontend/external/bookbrainz_db/author.py
+++ critiquebrainz/frontend/external/bookbrainz_db/author.py
@@ -2,10 +2,10 @@ import uuid
 from brainzutils import cache
 from typing import List
 import sqlalchemy
-import critiquebrainz.frontend.external.bookbrainz_db as db 
+import critiquebrainz.frontend.external.bookbrainz_db as db
 from critiquebrainz.frontend.external.bookbrainz_db import DEFAULT_CACHE_EXPIRATION
-from critiquebrainz.frontend.external.bookbrainz_db.identifiers import fetch_bb_external_identifiers
-from critiquebrainz.frontend.external.bookbrainz_db.relationships import fetch_relationships, AUTHOR_WORK_AUTHOR_REL_ID
+from critiquebrainz.frontend.external.bookbrainz_db.identifiers import process_bb_identifiers
+
 
 def get_author_by_bbid(bbid: str) -> dict:
 	"""
@@ -70,18 +70,65 @@ def fetch_multiple_authors(bbids: List[str]) -> dict:
 					author.ended,
 					gender.name as gender,
 					COALESCE (json_agg(area)
-                             FILTER (WHERE area IS NOT NULL),
-                             '[]'
-                             ) as area_info
+							 FILTER (WHERE area.name IS NOT NULL),
+							 '[]'
+							 ) as area_info,
+					COALESCE (json_agg(DISTINCT relationships)
+							 FILTER (WHERE relationships IS NOT NULL),
+							 '[]'
+							 ) as rels,
+					COALESCE (json_agg(DISTINCT identifiers)
+							 FILTER (WHERE identifiers IS NOT NULL),
+							 '[]'
+							 ) as identifiers
 			   FROM author
-		  LEFT JOIN musicbrainz.area area 
+		  LEFT JOIN musicbrainz.area area
 				 ON begin_area_id = area.id
 				 OR end_area_id = area.id
 				 OR area_id = area.id
-          LEFT JOIN musicbrainz.gender
-                 ON gender_id = gender.id
-			  WHERE bbid IN :bbids
-				AND master = 't'
+		  LEFT JOIN musicbrainz.gender
+				 ON gender_id = gender.id
+		  LEFT JOIN LATERAL (
+						 SELECT rel.id as id,
+								reltype.id as relationship_type_id,
+								reltype.label as label,
+								rel.source_bbid::text as source_bbid,
+								rel.target_bbid::text as target_bbid,
+								reltype.target_entity_type as target_entity_type,
+								reltype.source_entity_type as source_entity_type,
+								COALESCE(
+									jsonb_object_agg(relatttype.name, relatttext.text_value)
+									FILTER (WHERE relatts IS NOT NULL),
+									'[]'
+								) as attributes
+						   FROM relationship_set__relationship rels
+					  LEFT JOIN relationship rel ON rels.relationship_id = rel.id
+					  LEFT JOIN relationship_type reltype ON rel.type_id = reltype.id
+					  LEFT JOIN relationship_attribute_set__relationship_attribute relatts ON rel.attribute_set_id = relatts.set_id
+					  LEFT JOIN relationship_attribute relatt ON relatts.attribute_id = relatt.id
+					  LEFT JOIN relationship_attribute_type relatttype ON relatt.attribute_type = relatttype.id
+					  LEFT JOIN relationship_attribute_text_value relatttext ON relatts.attribute_id = relatttext.attribute_id
+						  WHERE rels.set_id = author.relationship_set_id
+					   GROUP BY rel.id,
+								reltype.id,
+								reltype.label,
+								rel.source_bbid,
+								rel.target_bbid,
+								reltype.target_entity_type,
+								reltype.source_entity_type
+                     ) AS relationships ON TRUE
+		  LEFT JOIN LATERAL (
+						 SELECT iden.type_id as type_id,
+								idtype.label as label,
+								idtype.display_template as url_template,
+								iden.value as value
+							FROM identifier_set__identifier idens
+						LEFT JOIN identifier iden on idens.identifier_id = iden.id
+						LEFT JOIN identifier_type idtype on iden.type_id = idtype.id
+							WHERE idens.set_id = author.identifier_set_id
+                     ) AS identifiers ON TRUE
+			   WHERE bbid IN :bbids
+				 AND master = 't'
 			GROUP BY bbid,
 					 author.name,
 					 sort_name,
@@ -107,8 +154,7 @@ def fetch_multiple_authors(bbids: List[str]) -> dict:
 			for author in authors:
 				author = dict(author)
 				author['bbid'] = str(author['bbid'])
-				author['identifiers'] = fetch_bb_external_identifiers(author['identifier_set_id'])
-				author['rels'] = fetch_relationships( author['relationship_set_id'], [AUTHOR_WORK_AUTHOR_REL_ID])
+				author['identifiers'] = process_bb_identifiers(author['identifiers'])
 				results[author['bbid']] = author
 
 			cache.set(bb_author_key, results, DEFAULT_CACHE_EXPIRATION)
--- critiquebrainz/frontend/external/bookbrainz_db/relationships.py
+++ critiquebrainz/frontend/external/bookbrainz_db/relationships.py
@@ -1,9 +1,3 @@
-from typing import List
-from brainzutils import cache
-import sqlalchemy
-import critiquebrainz.frontend.external.bookbrainz_db as db
-from critiquebrainz.frontend.external.bookbrainz_db import DEFAULT_CACHE_EXPIRATION
-
 AUTHOR_WORK_AUTHOR_REL_ID = 8
 EDITION_EDITION_GROUP_EDITION_REL_ID = 3
 EDITION_WORK_CONTAINS_REL_ID = 10
@@ -16,64 +10,3 @@ SERIES_REL_MAP = {
     'Publisher': 74,
     'Work': 71,
 }
-
-def fetch_relationships(relationship_set_id: int, relation_types_id: List) -> List:
-    """
-    Fetch relationships from the database.
-    Args:
-        relationship_set_id (int): ID of the relationship set.
-        relation_types_id (list): List of relation type ids.
-
-    Returns:
-        List of relationships of the given types.
-
-    """
-    if not relationship_set_id:
-        return []
-
-    key = cache.gen_key('bb_relationship', relationship_set_id, relation_types_id)
-    relationships = cache.get(key)
-    if not relationships:
-
-        query_params = {
-            'relationship_set_id': relationship_set_id,
-            'relation_types_id': tuple(relation_types_id),
-        }
-
-
-        with db.bb_engine.connect() as connection:
-            result = connection.execute(sqlalchemy.text("""
-                 SELECT rel.id as id,
-                        reltype.label as label,
-                        rel.source_bbid::text as source_bbid,
-                        rel.target_bbid::text as target_bbid,
-                        reltype.target_entity_type as target_entity_type,
-                        reltype.source_entity_type as source_entity_type,
-                        COALESCE( 
-                            jsonb_object_agg(relatttype.name, relatttext.text_value)
-                            FILTER (WHERE relatts IS NOT NULL),
-                            '[]'
-                        ) as attributes
-                   FROM relationship_set__relationship rels
-              LEFT JOIN relationship rel ON rels.relationship_id = rel.id
-              LEFT JOIN relationship_type reltype ON rel.type_id = reltype.id
-              LEFT JOIN relationship_attribute_set__relationship_attribute relatts ON rel.attribute_set_id = relatts.set_id
-              LEFT JOIN relationship_attribute relatt ON relatts.attribute_id = relatt.id
-              LEFT JOIN relationship_attribute_type relatttype ON relatt.attribute_type = relatttype.id
-              LEFT JOIN relationship_attribute_text_value relatttext ON relatts.attribute_id = relatttext.attribute_id
-                  WHERE rels.set_id = :relationship_set_id
-                    AND reltype.id in :relation_types_id
-               GROUP BY rel.id,
-                        reltype.label,
-                        rel.source_bbid,
-                        rel.target_bbid,
-                        reltype.target_entity_type,
-                        reltype.source_entity_type
-            """), query_params)
-            relationships = result.fetchall()
-            relationships = [dict(relationship) for relationship in relationships]
-            cache.set(key, relationships, DEFAULT_CACHE_EXPIRATION)
-
-    if not relationships:
-        return []
-    return relationships
